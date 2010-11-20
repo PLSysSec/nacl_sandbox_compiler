PLSysSec/nacl_sandbox_compiler

commit f687cf9074c408b5095ed852ee2492d5683621bf

Test suite names are validated in the SCons makefile.
Every test must belong to at least one major test suite.
Broken tests must be added to the 'broken_tests' suite.

Review URL: http://codereview.chromium.org/5059001

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@3715 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

SConstruct
@@ -266,10 +266,6 @@ pre_base_env.AddMethod(EnsureRequiredBuildWarnings)
 
 # ----------------------------------------------------------
 # Generic Test Wrapper
-# all test suites know so far
-TEST_SUITES = {'all_tests': None}
-
-
 
 # ----------------------------------------------------------
 # Add list of Flaky or Bad tests to skip per platform.  A
@@ -319,9 +315,78 @@ nacl_glibc_skiplist = [
     ]
 
 
-# ----------------------------------------------------------
-# Generic Test Wrapper
-# all test suites know so far
+# If a test is not in one of these suites, it will probally not be run on a
+# regular basis.  These are the suites that will be run by the try bot or that
+# a large number of users may run by hand.
+MAJOR_TEST_SUITES = set([
+  'small_tests',
+  'medium_tests',
+  'large_tests',
+  'small_tests_arm_hw_only',
+  'large_tests_arm_hw_only',
+  'browser_tests',
+  'huge_tests',
+
+  # Adding a test to broken_tests means the test will not be run unless
+  # explicitly requested by name. Broken tests cannot belong to any other test
+  # suite and are not added to all_tests
+  'broken_tests',
+])
+
+# These are the test suites we know exist, but aren't run on a regular basis.
+# These test suites are essentially shortcuts that run a specific subset of the
+# test cases.
+ACCEPTABLE_TEST_SUITES = set([
+  'validator_tests',
+  'validator_modeling',
+  'sel_ldr_tests',
+  'sel_ldr_sled_tests',
+  'barebones_tests',
+  'tsan_bot_tests',
+  'toolchain_tests',
+  ])
+
+# The major test suites are also acceptable names.  Suite names are checked
+# against this set in order to catch typos.
+ACCEPTABLE_TEST_SUITES.update(MAJOR_TEST_SUITES)
+
+
+def ValidateTestSuiteNames(suite_name, node_name):
+  if node_name is None:
+    node_name = '<unknown>'
+
+  # Prevent a silent failiure - strings are iterable!
+  if not isinstance(suite_name, (list, tuple)):
+    raise Exception, ("Test suites for %s should be specified as a list, " +
+      "not as a %s: %s") % (node_name, type(suite_name).__name__,
+      repr(suite_name))
+
+  if not suite_name:
+    raise Exception, ("No test suites are specified for %s. Put the test in " +
+      "'broken_tests' if there's a known issue and you don't want it to " +
+      "run.") % node_name
+
+  if 'broken_tests' in suite_name and len(suite_name) != 1:
+    raise Exception, ("'broken_tests' is a special test suite - if you're " +
+      "adding a test to it, that test (%s) should not be in any other " +
+      "suite.") % node_name
+
+  # Make sure each test is in at least one test suite we know will run
+  # 'broken_tests' is where you put tests you deliberately do not want to run
+  major_suites = set(suite_name).intersection(MAJOR_TEST_SUITES)
+  if not major_suites:
+    raise Exception, ("None of the test suites %s for %s are run on a " +
+    "regular basis.") % (repr(suite_name), node_name)
+
+  # Make sure a wierd test suite hasn't been inadvertantly specified
+  for s in suite_name:
+    if s not in ACCEPTABLE_TEST_SUITES:
+      raise Exception, ("\"%s\" is not a known test suite. Either this is " +
+      "a typo for %s, or it should be added to ACCEPTABLE_TEST_SUITES in " +
+      "SConstruct.") % (s, node_name)
+
+def IsBrokenTest(suite_name):
+  return len(suite_name) == 1 and 'broken_tests' == suite_name[0]
 
 def AddNodeToTestSuite(env, node, suite_name, node_name=None):
   build = env['BUILD_TYPE']
@@ -356,12 +421,15 @@ def AddNodeToTestSuite(env, node, suite_name, node_name=None):
     print '*** SKIPPING ', platform, ':', node_name
     return
 
+  ValidateTestSuiteNames(suite_name, node_name)
+
   AlwaysBuild(node)
-  env.Alias('all_tests', node)
+  if not IsBrokenTest(suite_name):
+    env.Alias('all_tests', node)
+
   for s in suite_name:
-    if s not in TEST_SUITES:
-      TEST_SUITES[s] = None
     env.Alias(s, node)
+
   if node_name:
     env.ComponentTestOutput(node_name, node)
 
@@ -372,7 +440,7 @@ pre_base_env.AddMethod(AddNodeToTestSuite)
 # NOTE: work around for scons non-determinism in the following two lines
 Alias('sel_ldr_sled_tests', [])
 
-Alias('small_tests', [ 'sel_ldr_sled_tests' ])
+Alias('small_tests', [])
 Alias('medium_tests', [])
 Alias('large_tests', [])
 Alias('browser_tests', [])
@@ -1698,9 +1766,7 @@ if (nacl_env['BUILD_SUBARCH'] == '32' and
     nacl_env['BUILD_ARCHITECTURE'] == 'x86'):
   nacl_env.Append(
       BUILD_SCONSCRIPTS = [
-      # This test assumes we have a compiler, which is not yet fully
-      # true for 64-bit.
-      'tools/tests/nacl.scons',
+        'tools/tests/nacl.scons',
       ])
 
 # ----------------------------------------------------------

src/trusted/service_runtime/nacl.scons
@@ -187,4 +187,6 @@ if (env['TARGET_ARCHITECTURE'] == env['BUILD_ARCHITECTURE']
           base_name + '.out',
           command=[env.File(nexe_name)],
           exit_status=expected_exit_status)
-      env.AddNodeToTestSuite(node, ['sel_ldr_sled_tests'], 'run_' + base_name)
+      env.AddNodeToTestSuite(node,
+                              ['small_tests', 'sel_ldr_sled_tests'],
+                              'run_' + base_name)

tests/barebones/nacl.scons
@@ -12,9 +12,11 @@ Import('env')
 # TODO(mseaborn): To fix this, either switch these tests to
 # dynamically link against libc, or make use of ld.so optional.
 if env.Bit('nacl_glibc'):
-  TEST_SUITES = []
+  TEST_SUITES = ['broken_tests']
+  TEST_SUITES_BARE = ['broken_tests']
 else:
-  TEST_SUITES = ['small_tests']
+  TEST_SUITES = ['small_tests', 'sel_ldr_tests']
+  TEST_SUITES_BARE = ['small_tests', 'sel_ldr_tests', 'barebones_tests']
 
 LINK_FLAGS_MULTILIB = ['-nostdlib']
 
@@ -74,7 +76,7 @@ for basename in ['addr_modes',
       command=[env.File(nexe)],
       exit_status='55',)
   env.AddNodeToTestSuite(node,
-                         TEST_SUITES + ['barebones_tests', 'sel_ldr_tests'],
+                         TEST_SUITES_BARE,
                          test)
 
 
@@ -87,5 +89,5 @@ if env['BUILD_ARCHITECTURE'] == 'arm' and not ARGUMENTS.get('bitcode'):
       command=[env.File(nexe)],
       exit_status='55',)
   env.AddNodeToTestSuite(node,
-                         TEST_SUITES + ['sel_ldr_tests'],
+                         TEST_SUITES,
                          'run_gold-barebones_hello_world_test')

tests/data_not_executable/nacl.scons
@@ -25,7 +25,7 @@ if env['TARGET_ARCHITECTURE'] == 'arm':
   # TODO(mseaborn): Re-enable when the ARM Buildbot slave has been
   # upgraded to a newer kernel.
   # test_suites = ['small_tests_arm_hw_only']
-  test_suites = []
+  test_suites = ['broken_tests']
 else:
   test_suites = ['small_tests']
 

tests/syscalls/nacl.scons
@@ -127,7 +127,8 @@ node = env.CommandSelLdrTestNacl(
 #TODO(dmichael):  Add this back to small_tests or to
 # medium_tests when the error conditions are fixed, c.f.:
 # http://code.google.com/p/nativeclient/issues/detail?id=848
-env.AddNodeToTestSuite(node, ['sel_ldr_tests'], 'run_semaphore_tests')
+# Also add it back to 'sel_ldr_tests' when it is fixed
+env.AddNodeToTestSuite(node, ['broken_tests'], 'run_semaphore_tests')
 
 env.ComponentProgram('mem_test.nexe',
                      ['mem_test.cc'],

tests/toolchain/nacl.scons
@@ -24,8 +24,9 @@ if not ARGUMENTS.get('bitcode') and not env.Bit('nacl_glibc'):
 nexe = wa_env.ComponentProgram('dummy.nexe', 'dummy.c')
 node = wa_env.CommandValidatorTestNacl('whole_archive_test.out',
                                        image=[nexe])
-wa_env.AddNodeToTestSuite(node, ['toolchain_tests'], 'run_whole_archive_test')
-
+wa_env.AddNodeToTestSuite(node,
+                          ['toolchain_tests', 'small_tests'],
+                          'run_whole_archive_test')
 
 nexe = env.ComponentProgram('setlongjmp.nexe', 'setlongjmp.c')
 node = env.CommandSelLdrTestNacl('setlongjmp.out',
@@ -38,9 +39,10 @@ env.AddNodeToTestSuite(node,
 
 # NOTE: this currently only works for PNaCl, c.f:
 # http://code.google.com/p/nativeclient/issues/detail?id=968
-INITFINIT_TESTS=['toolchain_tests', 'small_tests']
-if not ARGUMENTS.get('bitcode'):
-  INITFINIT_TESTS=[]
+if ARGUMENTS.get('bitcode'):
+  INITFINIT_TESTS=['toolchain_tests', 'small_tests']
+else:
+  INITFINIT_TESTS=['broken_tests']
 
 nexe = env.ComponentProgram('initfini.nexe', 'initfini.c')
 node = env.CommandSelLdrTestNacl('initfini.out',
@@ -113,13 +115,13 @@ for tag, e in ENVIRONMENTS_TO_TRY:
       # this test needs a lot more work for ARM and does not even compile
       if  src =='stack_frame.cc':
         continue
-      tests = []
+      tests = ['broken_tests']
     # http://code.google.com/p/nativeclient/issues/detail?id=1106
     if IsPicBuild(e):
-      tests = []
+      tests = ['broken_tests']
     # the tracing gets messed up by inlining
     if src =='unwind_trace.cc' and tag.startswith('opt'):
-      tests = []
+      tests = ['broken_tests']
     name = src.split('.')[0] + '_' + tag
     nobj = e.ComponentObject(name + '.o', src)
     nexe = e.ComponentProgram(name + '.nexe', nobj)

commit 03c544ba8af686a371e8e6dc7ccbb919e404b0f8

Put run_whole_archive_test in the correct list, to disable it with nacl-glibc
I made a silly mistake in r3743 and added it to the wrong list in
SConstruct!

This fixes a test failure introduced by r3715, which added this test
to small_tests.

BUG=http://code.google.com/p/nativeclient/issues/detail?id=1183
TEST=small_tests --nacl_glibc

Review URL: http://codereview.chromium.org/5172010

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@3746 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

SConstruct
@@ -146,12 +146,6 @@ ACCEPTABLE_ARGUMENTS = set([
     # makes it easier to build x86-32 NaCl on x86-64 Ubuntu Linux,
     # where there is no -dev package for the 32-bit libcrypto.
     'use_libcrypto',
-    # run_whole_archive_test is disabled because it runs ncval.  ncval
-    # does not pass on dynamically-linked executables because, to
-    # reduce executable size, the code segment is not page-aligned,
-    # and ncval rejects such executables.
-    # See http://code.google.com/p/nativeclient/issues/detail?id=1183
-    'run_whole_archive_test',
   ])
 
 def CheckArguments():
@@ -315,6 +309,12 @@ nacl_glibc_skiplist = [
     'sdk_minimal_test',
     # TODO(elijahtaylor) add apropriate syscall hooks for glibc
     'run_gc_instrumentation_test',
+    # run_whole_archive_test is disabled because it runs ncval.  ncval
+    # does not pass on dynamically-linked executables because, to
+    # reduce executable size, the code segment is not page-aligned,
+    # and ncval rejects such executables.
+    # See http://code.google.com/p/nativeclient/issues/detail?id=1183
+    'run_whole_archive_test',
     ]
 
 