PLSysSec/nacl_sandbox_compiler

commit d83bde30e1e93bea3bbadfcfc6119b4cd85a6a15

sel_ldr: Use single VirtualAlloc() calls to map guard regions on 64-bit Windows
This will make startup faster.

Previously, each 64k page of the 40GB guard regions was mapped with a
separate VirtualAlloc() call.  This resulted in 1310720 VirtualAlloc()
calls, which caused address space allocation to take around 1 second.

We can fix this by doing just two VirtualAlloc() calls, because the
pages in the guard regions do not need to be independently remapped
(unlike the main 4GB part of untrusted address space).

We create sel_addrspace_win_x86_64.c with the code that is specific to
both x86-64 and Windows.

We create sel_addrspace_posix_x86_64.c and move some functions here:
 * Move the POSIX version of NaClAllocatePow2AlignedMemory() here.
   We no longer compile this function in on x86-32, where it was not
   used.  The Windows version is no longer needed.
 * Move the original x86-64 implementation of NaClAllocateSpace() here.
   This is now used only on POSIX.

Extend mmap_test.c to check that the guard regions are correctly
mapped on Windows.  Give this test a name in Scons so that it can be
run directly.

BUG=http://code.google.com/p/nativeclient/issues/detail?id=1876
TEST=run_trusted_mmap_test

Review URL: http://codereview.chromium.org/6982044

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@5549 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

src/trusted/service_runtime/arch/x86_64/sel_addrspace_posix_x86_64.c
@@ -1,0 +1,136 @@
+/*
+ * Copyright (c) 2011 The Native Client Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include <errno.h>
+#include <sys/mman.h>
+
+#include "native_client/src/shared/platform/nacl_check.h"
+#include "native_client/src/trusted/service_runtime/sel_ldr.h"
+
+
+#define FOURGIG     (((size_t) 1) << 32)
+#define GUARDSIZE   (10 * FOURGIG)
+#define ALIGN_BITS  32
+#define MSGWIDTH    "25"
+
+
+/*
+ * NaClAllocatePow2AlignedMemory is for allocating a large amount of
+ * memory of mem_sz bytes that must be address aligned, so that
+ * log_alignment low-order address bits must be zero.
+ *
+ * Returns the aligned region on success, or NULL on failure.
+ */
+static void *NaClAllocatePow2AlignedMemory(size_t mem_sz,
+                                           size_t log_alignment) {
+  uintptr_t pow2align;
+  size_t    request_sz;
+  void      *mem_ptr;
+  uintptr_t orig_addr;
+  uintptr_t rounded_addr;
+  size_t    extra;
+
+  pow2align = ((uintptr_t) 1) << log_alignment;
+
+  request_sz = mem_sz + pow2align;
+
+  NaClLog(4,
+          "%"MSGWIDTH"s %016"NACL_PRIxS"\n",
+          " Ask:",
+          request_sz);
+
+  mem_ptr = mmap((void *) 0,
+           request_sz,
+           PROT_NONE,
+           MAP_ANONYMOUS | MAP_NORESERVE | MAP_PRIVATE,
+           -1,
+           (off_t) 0);
+  if (MAP_FAILED == mem_ptr) {
+    return NULL;
+  }
+  orig_addr = (uintptr_t) mem_ptr;
+
+  NaClLog(4,
+          "%"MSGWIDTH"s %016"NACL_PRIxPTR"\n",
+          "orig memory at",
+          orig_addr);
+
+  rounded_addr = (orig_addr + (pow2align - 1)) & ~(pow2align - 1);
+  extra = rounded_addr - orig_addr;
+
+  if (0 != extra) {
+    NaClLog(4,
+            "%"MSGWIDTH"s %016"NACL_PRIxPTR", %016"NACL_PRIxS"\n",
+            "Freeing front:",
+            orig_addr,
+            extra);
+    if (-1 == munmap((void *) orig_addr, extra)) {
+      perror("munmap (front)");
+      NaClLog(LOG_FATAL,
+              "NaClAllocatePow2AlignedMemory: munmap front failed\n");
+    }
+  }
+
+  extra = pow2align - extra;
+  if (0 != extra) {
+    NaClLog(4,
+            "%"MSGWIDTH"s %016"NACL_PRIxPTR", %016"NACL_PRIxS"\n",
+            "Freeing tail:",
+            rounded_addr + mem_sz,
+            extra);
+    if (-1 == munmap((void *) (rounded_addr + mem_sz),
+         extra)) {
+      perror("munmap (end)");
+      NaClLog(LOG_FATAL,
+              "NaClAllocatePow2AlignedMemory: munmap tail failed\n");
+    }
+  }
+  NaClLog(4,
+          "%"MSGWIDTH"s %016"NACL_PRIxPTR"\n",
+          "Aligned memory:",
+          rounded_addr);
+
+  /*
+   * we could also mmap again at rounded_addr w/o MAP_NORESERVE etc to
+   * ensure that we have the memory, but that's better done in another
+   * utility function.  the semantics here is no paging space
+   * reserved, as in Windows MEM_RESERVE without MEM_COMMIT.
+   */
+
+  return (void *) rounded_addr;
+}
+
+NaClErrorCode NaClAllocateSpace(void **mem, size_t addrsp_size) {
+  size_t        mem_sz = 2 * GUARDSIZE + FOURGIG;  /* 40G guard on each side */
+  size_t        log_align = ALIGN_BITS;
+  void          *mem_ptr;
+
+  NaClLog(4, "NaClAllocateSpace(*, 0x%016"NACL_PRIxS" bytes).\n",
+          addrsp_size);
+
+  CHECK(addrsp_size == FOURGIG);
+
+  errno = 0;
+  mem_ptr = NaClAllocatePow2AlignedMemory(mem_sz, log_align);
+  if (NULL == mem_ptr) {
+    if (0 != errno) {
+      perror("NaClAllocatePow2AlignedMemory");
+    }
+    NaClLog(LOG_WARNING, "Memory allocation failed\n");
+
+    return LOAD_NO_MEMORY;
+  }
+  /*
+   * The module lives in the middle FOURGIG of the allocated region --
+   * we skip over an initial 40G guard.
+   */
+  *mem = (void *) (((char *) mem_ptr) + GUARDSIZE);
+  NaClLog(4,
+          "NaClAllocateSpace: addr space at 0x%016"NACL_PRIxPTR"\n",
+          (uintptr_t) *mem);
+
+  return LOAD_OK;
+}

src/trusted/service_runtime/arch/x86_64/sel_addrspace_win_x86_64.c
@@ -1,0 +1,80 @@
+/*
+ * Copyright (c) 2011 The Native Client Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "native_client/src/shared/platform/nacl_check.h"
+#include "native_client/src/shared/platform/nacl_find_addrsp.h"
+#include "native_client/src/trusted/service_runtime/sel_ldr.h"
+
+
+#define FOURGIG     (((size_t) 1) << 32)
+#define ALIGN_BITS  32
+#define GUARDSIZE   (10 * FOURGIG)
+
+
+static uintptr_t NaClFindAddressSpacePow2Aligned(size_t mem_sz,
+                                                 size_t log_alignment) {
+  uintptr_t pow2align = ((uintptr_t) 1) << log_alignment;
+  size_t request_size = mem_sz + pow2align;
+  uintptr_t unrounded_addr;
+
+  if (!NaClFindAddressSpace(&unrounded_addr, request_size)) {
+    NaClLog(LOG_FATAL,
+            "NaClFindAddressSpacePow2Aligned: Failed to reserve %"NACL_PRIxS
+            " bytes of address space\n",
+            request_size);
+  }
+  return (unrounded_addr + (pow2align - 1)) & ~(pow2align - 1);
+}
+
+NaClErrorCode NaClAllocateSpace(void **mem, size_t addrsp_size) {
+  size_t mem_sz = 2 * GUARDSIZE + FOURGIG;  /* 40G guard on each side */
+  uintptr_t rounded_addr;
+  void *request_addr;
+  void *allocated;
+  uintptr_t mem_ptr;
+
+  CHECK(addrsp_size == FOURGIG);
+
+  rounded_addr = NaClFindAddressSpacePow2Aligned(mem_sz, ALIGN_BITS);
+
+  /*
+   * Reserve the guard regions.  We can map these with a single
+   * VirtualAlloc() each because we do not need to remap any pages
+   * inside the guard regions.
+   */
+  request_addr = (void *) rounded_addr;
+  allocated = VirtualAlloc(request_addr, GUARDSIZE, MEM_RESERVE, PAGE_NOACCESS);
+  if (allocated != request_addr) {
+    NaClLog(LOG_FATAL,
+            "NaClAllocateSpace: Failed to reserve first guard region\n");
+  }
+  request_addr = (void *) (rounded_addr + GUARDSIZE + FOURGIG);
+  allocated = VirtualAlloc(request_addr, GUARDSIZE, MEM_RESERVE, PAGE_NOACCESS);
+  if (allocated != request_addr) {
+    NaClLog(LOG_FATAL,
+            "NaClAllocateSpace: Failed to reserve second guard region\n");
+  }
+  /*
+   * Reserve the main part of address space.  Each page needs to be
+   * mapped with a separate VirtualAlloc() call so that it can be
+   * independently remapped later.
+   */
+  for (mem_ptr = rounded_addr + GUARDSIZE;
+       mem_ptr < rounded_addr + GUARDSIZE + FOURGIG;
+       mem_ptr += NACL_MAP_PAGESIZE) {
+    allocated = VirtualAlloc((void *) mem_ptr,
+                             NACL_MAP_PAGESIZE,
+                             MEM_RESERVE,
+                             PAGE_NOACCESS);
+    if ((uintptr_t) allocated != mem_ptr) {
+      NaClLog(LOG_FATAL,
+              "NaClAllocateSpace: Failed to reserve page at %"NACL_PRIxPTR"\n",
+              mem_ptr);
+    }
+  }
+  *mem = (void *) (rounded_addr + GUARDSIZE);
+  return LOAD_OK;
+}

src/trusted/service_runtime/arch/x86_64/sel_addrspace_x86_64.c
@@ -13,44 +13,8 @@
 #include "native_client/src/trusted/service_runtime/sel_memory.h"
 #include "native_client/src/trusted/service_runtime/sel_ldr.h"
 
-#define ALIGN_BITS  32
 #define FOURGIG     (((size_t) 1) << 32)
-#define FOURKAY     (((size_t) 1) << 12)
 #define GUARDSIZE   (10 * FOURGIG)
-#define MSGWIDTH    "25"
-
-NaClErrorCode NaClAllocateSpace(void **mem, size_t addrsp_size) {
-  size_t        mem_sz = 2 * GUARDSIZE + FOURGIG;  /* 40G guard on each side */
-  size_t        log_align = ALIGN_BITS;
-  void          *mem_ptr;
-
-  NaClLog(4, "NaClAllocateSpace(*, 0x%016"NACL_PRIxS" bytes).\n",
-          addrsp_size);
-
-  CHECK(addrsp_size == FOURGIG);
-
-  errno = 0;
-  mem_ptr = NaClAllocatePow2AlignedMemory(mem_sz, log_align);
-  if (NULL == mem_ptr) {
-    if (0 != errno) {
-      perror("NaClAllocatePow2AlignedMemory");
-    }
-    NaClLog(LOG_WARNING, "Memory allocation failed\n");
-
-    return LOAD_NO_MEMORY;
-  }
-  /*
-   * The module lives in the middle FOURGIG of the allocated region --
-   * we skip over an initial 40G guard.
-   */
-  *mem = (void *) (((char *) mem_ptr) + GUARDSIZE);
-  NaClLog(4,
-          "NaClAllocateSpace: addr space at 0x%016"NACL_PRIxPTR"\n",
-          (uintptr_t) *mem);
-
-
-  return LOAD_OK;
-}
 
 NaClErrorCode NaClMprotectGuards(struct NaClApp *nap) {
   uintptr_t start_addr;

src/trusted/service_runtime/arch/x86_64/service_runtime_x86_64.gyp
@@ -53,16 +53,19 @@
         ['OS=="mac"', {
           'sources' : [
             '../../osx/nacl_signal_64.c',
+            'sel_addrspace_posix_x86_64.c',
           ] },
         ],
         ['OS=="linux"', {
           'sources' : [
             '../../linux/nacl_signal_64.c',
+            'sel_addrspace_posix_x86_64.c',
           ] },
         ],
         ['OS=="win"', {
           'sources' : [
             '../../win/nacl_signal_64.c',
+            'sel_addrspace_win_x86_64.c',
           ] },
         ],
         [ 'library=="shared_library"', {

src/trusted/service_runtime/build.scons
@@ -128,6 +128,10 @@ elif env.Bit('build_x86_64'):
       'arch/x86_64/sel_rt_64.c',
       'arch/x86_64/tramp_64.S',
       ]
+  if env.Bit('windows'):
+    ldr_inputs += ['arch/x86_64/sel_addrspace_win_x86_64.c']
+  else:
+    ldr_inputs += ['arch/x86_64/sel_addrspace_posix_x86_64.c']
 elif env.Bit('build_arm'):
   ldr_inputs += [
     'arch/arm/nacl_app.c',
@@ -416,7 +420,7 @@ if ((not env.Bit('windows') or not env.Bit('coverage_enabled')) and
       osenv='NACL_DISABLE_DYNAMIC_LOADING=1')
   # This test fails for arm on 32-bit machines running qemu.
   # So don't run it there.
-  env.AddNodeToTestSuite(node, ['medium_tests'],
+  env.AddNodeToTestSuite(node, ['medium_tests'], 'run_trusted_mmap_test',
                          is_broken=env.Bit('build_arm') and env.UsingEmulator())
 
 

src/trusted/service_runtime/linux/sel_memory.c
@@ -28,9 +28,6 @@
 #include "native_client/src/trusted/service_runtime/include/machine/_types.h"
 
 
-#define MSGWIDTH  "25"
-
-
 void NaCl_page_free(void     *p,
                     size_t   size) {
   if (p == 0 || size == 0)
@@ -131,83 +128,4 @@ int NaCl_madvise(void           *start,
    * MADV_DONTNEED and MADV_NORMAL are needed
    */
   return ret == -1 ? -errno : ret;
-}
-
-
-void *NaClAllocatePow2AlignedMemory(size_t mem_sz, size_t log_alignment) {
-  uintptr_t pow2align;
-  size_t    request_sz;
-  void      *mem_ptr;
-  uintptr_t orig_addr;
-  uintptr_t rounded_addr;
-  size_t    extra;
-
-  pow2align = ((uintptr_t) 1) << log_alignment;
-
-  request_sz = mem_sz + pow2align;
-
-  NaClLog(4,
-          "%"MSGWIDTH"s %016"NACL_PRIxS"\n",
-          " Ask:",
-          request_sz);
-
-  mem_ptr = mmap((void *) 0,
-           request_sz,
-           PROT_NONE,
-           MAP_ANONYMOUS | MAP_NORESERVE | MAP_PRIVATE,
-           -1,
-           (off_t) 0);
-  if (MAP_FAILED == mem_ptr) {
-    return NULL;
-  }
-  orig_addr = (uintptr_t) mem_ptr;
-
-  NaClLog(4,
-          "%"MSGWIDTH"s %016"NACL_PRIxPTR"\n",
-          "orig memory at",
-          orig_addr);
-
-  rounded_addr = (orig_addr + (pow2align - 1)) & ~(pow2align - 1);
-  extra = rounded_addr - orig_addr;
-
-  if (0 != extra) {
-    NaClLog(4,
-            "%"MSGWIDTH"s %016"NACL_PRIxPTR", %016"NACL_PRIxS"\n",
-            "Freeing front:",
-            orig_addr,
-            extra);
-    if (-1 == munmap((void *) orig_addr, extra)) {
-      perror("munmap (front)");
-      NaClLog(LOG_FATAL,
-              "NaClAllocatePow2AlignedMemory: munmap front failed\n");
-    }
-  }
-
-  extra = pow2align - extra;
-  if (0 != extra) {
-    NaClLog(4,
-            "%"MSGWIDTH"s %016"NACL_PRIxPTR", %016"NACL_PRIxS"\n",
-            "Freeing tail:",
-            rounded_addr + mem_sz,
-            extra);
-    if (-1 == munmap((void *) (rounded_addr + mem_sz),
-         extra)) {
-      perror("munmap (end)");
-      NaClLog(LOG_FATAL,
-              "NaClAllocatePow2AlignedMemory: munmap tail failed\n");
-    }
-  }
-  NaClLog(4,
-          "%"MSGWIDTH"s %016"NACL_PRIxPTR"\n",
-          "Aligned memory:",
-          rounded_addr);
-
-  /*
-   * we could also mmap again at rounded_addr w/o MAP_NORESERVE etc to
-   * ensure that we have the memory, but that's better done in another
-   * utility function.  the semantics here is no paging space
-   * reserved, as in Windows MEM_RESERVE without MEM_COMMIT.
-   */
-
-  return (void *) rounded_addr;
 }

src/trusted/service_runtime/mmap_test.c
@@ -49,6 +49,35 @@ void CheckLowerMappings(struct NaClVmmap *mem_map) {
             NACL_ABI_PROT_READ | NACL_ABI_PROT_WRITE);
 }
 
+#if NACL_ARCH(NACL_BUILD_ARCH) == NACL_x86 && NACL_BUILD_SUBARCH == 64
+void CheckForGuardRegion(uintptr_t addr, size_t expected_size) {
+  /*
+   * This check is easiest to do on Windows where we can query memory
+   * mappings with VirtualQuery().  On Linux this would involve
+   * parsing /proc/self/maps.
+   */
+#if NACL_WINDOWS
+  MEMORY_BASIC_INFORMATION info;
+  size_t result = VirtualQuery((void *) addr, &info, sizeof(info));
+  ASSERT_EQ(result, sizeof(info));
+  ASSERT_EQ(info.BaseAddress, (void *) addr);
+  ASSERT_EQ(info.AllocationBase, (void *) addr);
+  ASSERT_EQ(info.RegionSize, expected_size);
+  /*
+   * We certainly do not expect MEM_COMMIT here, because usually we
+   * would not be able to allocate 80GB of swap space.
+   */
+  ASSERT_EQ(info.State, MEM_RESERVE);
+  /* Note that Windows returns 0 in place of PAGE_NOACCESS (1) here. */
+  ASSERT_EQ(info.Protect, 0);
+  ASSERT_EQ(info.Type, MEM_PRIVATE);
+#else
+  UNREFERENCED_PARAMETER(addr);
+  UNREFERENCED_PARAMETER(expected_size);
+#endif
+}
+#endif
+
 int main(int argc, char **argv) {
   char *nacl_file;
   struct GioMemoryFileSnapshot gf;
@@ -300,6 +329,13 @@ int main(int argc, char **argv) {
 
   errcode = NaClSysMunmap(natp, (void *) (uintptr_t) initial_addr, 0);
   ASSERT_EQ(errcode, -NACL_ABI_EINVAL);
+
+#if NACL_ARCH(NACL_BUILD_ARCH) == NACL_x86 && NACL_BUILD_SUBARCH == 64
+  CheckForGuardRegion(nap->mem_start - ((size_t) 40 << 30),
+                      (size_t) 40 << 30);
+  CheckForGuardRegion(nap->mem_start + ((size_t) 4 << 30),
+                      (size_t) 40 << 30);
+#endif
 
   printf("PASS\n");
   return 0;

src/trusted/service_runtime/sel_memory.h
@@ -44,15 +44,6 @@ int   NaCl_madvise(void           *start,
                    size_t         length,
                    int            advice) NACL_WUR;
 
-/*
- * NaClAllocatePow2AlignedMemory is for allocating a large amount of
- * memory of mem_sz bytes that must be address aligned, so that
- * log_alignment low-order address bits must be zero.
- *
- * Returns the aligned region on success, or NULL on failure.
- */
-void *NaClAllocatePow2AlignedMemory(size_t mem_sz, size_t log_alignment);
-
 
 #ifdef __cplusplus
 }

commit 4aaa425974402aabd0a7202e32ee688e0d6dd8ae

Fix build failure of x86-64 sel_ldr on Mac, introduced by r5549
The Mac headers define MAP_ANON but not MAP_ANONYMOUS.  Use
nacl_platform.h to get the alias defined.

BUG=http://code.google.com/p/nativeclient/issues/detail?id=1876
TEST="run_hello_world_test platform=x86-64" on Mac

Review URL: http://codereview.chromium.org/7150016

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@5644 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

src/trusted/service_runtime/arch/x86_64/sel_addrspace_posix_x86_64.c
@@ -7,6 +7,7 @@
 #include <errno.h>
 #include <sys/mman.h>
 
+#include "native_client/src/include/nacl_platform.h"
 #include "native_client/src/shared/platform/nacl_check.h"
 #include "native_client/src/trusted/service_runtime/sel_ldr.h"
 