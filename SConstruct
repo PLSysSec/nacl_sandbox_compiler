PLSysSec/nacl_sandbox_compiler

commit 0cba7e7d9a617548f757b548d04625fba9e0a0c8

Partial-sdk retirement plan: update tests to reference GLES2 headers from the source tree instead of from the toolchain include directory. Review URL: http://codereview.chromium.org/7112009
git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@5551 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

SConstruct
@@ -2643,7 +2643,9 @@ nacl_extra_sdk_env = pre_base_env.Clone(
     BUILD_TYPE_DESCRIPTION = 'NaCl SDK extra library build',
     NACL_BUILD_FAMILY = 'UNTRUSTED',
     IRT_DATA_REGION_START = nacl_env['IRT_DATA_REGION_START'],
-    CPPPATH = ['${SOURCE_ROOT}'],
+    # ${SOURCE_ROOT} for #include <ppapi/...>
+    # ${SOURCE_ROOT}/gpu for #include <GLES2/...>"
+    CPPPATH = ['${SOURCE_ROOT}', '${SOURCE_ROOT}/gpu'],
     CPPDEFINES = [
       ['NACL_BUILD_ARCH', '${BUILD_ARCHITECTURE}' ],
       ['NACL_BUILD_SUBARCH', '${BUILD_SUBARCH}' ],

tests/ppapi/nacl.scons
@@ -37,6 +37,7 @@ def EmitHeaderTest(target=None, source=None, env=None):
   return 0
 
 
+env.Append(CPPPATH=['${SOURCE_ROOT}/gpu'])
 cpp_header_test_cc = env.Command(
     '$OBJ_ROOT/gen/native_client/test/ppapi/cpp_header_test.cc',
     [], Action(EmitHeaderTest, varlist=['TEST_HEADERS']),

tests/ppapi_gles_book/nacl.scons
@@ -20,9 +20,10 @@ env.Append(CFLAGS=['-Wno-pointer-sign'])
 # ..and don't warn about missing braces
 env.Append(CCFLAGS=['-Wno-missing-braces'])
 
-# Add local path to find GPU sources.
+# Add local path to find GPU sources and GLES2 includes.
 env.Append(CPPPATH=[
     '$SOURCE_ROOT/third_party/gles2_book/Common/Include',
+    '$SOURCE_ROOT/gpu',
     ])
 
 gles_demo_srcs = [
@@ -65,4 +66,3 @@ for demo, sources in gles_book_examples.iteritems():
                     'm'])
     env.Publish('ppapi_' + demo + '.nexe', 'run',
        ['ppapi_gles_book.html?nexe=' + demo ])
-

commit 74dfaa2b2ced5ab01f2df6d574e1742cdaedd350

Use the standard distro GLES2 headers.
This change builds libppapi_gles2 using the standard GLES2 headers, instead of
the locally modified ones in gpu/GLES2.  This produces the correct symbols to
link with when compiling OpenGL code using the headers we ship.

Note: This is a regression from r5551

BUG= http://code.google.com/p/nativeclient/issues/detail?id=1978
TEST=chrome_browser_tests

Review URL: http://codereview.chromium.org/7316006

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@5917 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

SConstruct
@@ -2424,8 +2424,7 @@ nacl_common_env = pre_base_env.Clone(
     ARFLAGS = 'rc',
 
     # ${SOURCE_ROOT} for #include <ppapi/...>
-    # ${SOURCE_ROOT}/gpu for #include <GLES2/...>
-    CPPPATH = ['${SOURCE_ROOT}', '${SOURCE_ROOT}/gpu'],
+    CPPPATH = ['${SOURCE_ROOT}'],
 
     EXTRA_CFLAGS = [],
     EXTRA_CCFLAGS = ARGUMENTS.get('nacl_ccflags', '').split(':'),

src/shared/ppapi/nacl.scons
@@ -31,7 +31,9 @@ env.NaClSdkLibrary('ppapi_cpp', cpp_sources, LIBS=['nacl'])
 env.AddLibraryToSdk(['ppapi_cpp'])
 
 env.Append(CPPPATH=[
-    '$SOURCE_ROOT/ppapi/lib/gl/gles2'])
+    '$SOURCE_ROOT/ppapi/lib/gl/gles2',
+    '$SOURCE_ROOT/ppapi/lib/gl/include',
+    ])
 
 # Load ppapi_gl.gypi
 ppapi_gl_gypi = gyp_extract.LoadGypFile(

tests/ppapi/nacl.scons
@@ -37,7 +37,7 @@ def EmitHeaderTest(target=None, source=None, env=None):
   return 0
 
 
-env.Append(CPPPATH=['${SOURCE_ROOT}/gpu'])
+env.Append(CPPPATH=['${SOURCE_ROOT}/ppapi/lib/gl/include'])
 cpp_header_test_cc = env.Command(
     '$OBJ_ROOT/gen/native_client/test/ppapi/cpp_header_test.cc',
     [], Action(EmitHeaderTest, varlist=['TEST_HEADERS']),

tests/ppapi_gles_book/nacl.scons
@@ -23,7 +23,7 @@ env.Append(CCFLAGS=['-Wno-missing-braces'])
 # Add local path to find GPU sources and GLES2 includes.
 env.Append(CPPPATH=[
     '$SOURCE_ROOT/third_party/gles2_book/Common/Include',
-    '$SOURCE_ROOT/gpu',
+    '$SOURCE_ROOT/ppapi/lib/gl/include',
     ])
 
 gles_demo_srcs = [