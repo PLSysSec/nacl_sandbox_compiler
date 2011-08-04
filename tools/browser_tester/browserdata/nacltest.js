PLSysSec/nacl_sandbox_compiler

commit f90ffe66310bf13cee73db0c9003c070051561f7

Added a stress test for determining now many nexes can be loaded simultaneously.
This is a manual test that can be used to probe resource exhaustion failure modes.

BUG= http://code.google.com/p/nativeclient/issues/detail?id=2091
TEST= ./scons run_browser_stress_test

Review URL: http://codereview.chromium.org/7551001

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@6320 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

tools/browser_tester/browserdata/nacltest.js
@@ -8,6 +8,22 @@ function $(id) {
 }
 
 
+function createNaClEmbed(args) {
+  var fallback = function(value, default_value) {
+    return value !== undefined ? value : default_value;
+  };
+  var embed = document.createElement('embed');
+  embed.id = args.id;
+  embed.src = args.src;
+  embed.type = fallback(args.type, 'application/x-nacl');
+  // JavaScript inconsistency: this is equivalent to class=... in HTML.
+  embed.className = fallback(args.className, 'naclModule');
+  embed.width = fallback(args.width, 0);
+  embed.height = fallback(args.height, 0);
+  return embed;
+}
+
+
 function exceptionToLogText(e) {
   if (typeof e == 'object' && 'message' in e && 'stack' in e) {
     return e.message + '\n' + e.stack.toString();
@@ -668,13 +684,12 @@ function TestStatus(tester, name, async) {
   this.expectMessageSequence = function(plugin, messages, callback) {
     this.assert(messages.length > 0, 'Must provide at least one message');
     var local_messages = messages.slice();
-    var listener = this.wrap(function(message) {
+    var listener = function(message) {
       // skip debug messages
       if (message.data.indexOf('@:') == 0) {
         this_.log('DEBUG: ' + message.data.substr(2));
         return;
       }
-      plugin.removeEventListener('message', listener, false);
       this_.assertEqual(message.data, local_messages.shift());
       if (callback !== undefined) {
         callback(message.data);
@@ -682,10 +697,18 @@ function TestStatus(tester, name, async) {
       if (local_messages.length == 0) {
         this_.pass();
       } else {
-        plugin.addEventListener('message', listener, false);
+        this_.expectEvent(plugin, 'message', listener);
       }
+    }
+    this.expectEvent(plugin, 'message', listener);
+  }
+
+  this.expectEvent = function(src, event_type, listener) {
+    var wrapper = this.wrap(function(e) {
+      src.removeEventListener(event_type, wrapper, false);
+      listener(e);
     });
-    plugin.addEventListener('message', listener, false);
+    src.addEventListener(event_type, wrapper, false);
   }
 }
 

SConstruct
@@ -1141,6 +1141,7 @@ def PPAPIBrowserTester(env,
                        files,
                        map_files=(),
                        extensions=(),
+                       timeout=20,
                        log_verbosity=2,
                        args=(),
                        # list of "--flag=value" pairs (no spaces!)
@@ -1153,7 +1154,6 @@ def PPAPIBrowserTester(env,
   env = env.Clone()
   SetupBrowserEnv(env)
 
-  timeout = 20
   if 'scale_timeout' in ARGUMENTS:
     timeout = timeout * int(ARGUMENTS['scale_timeout'])
 
@@ -2721,6 +2721,7 @@ nonvariant_tests = [
     'tests/ppapi_browser/ppp_input_event/nacl.scons',
     'tests/ppapi_browser/ppp_instance/nacl.scons',
     'tests/ppapi_browser/progress_events/nacl.scons',
+    'tests/ppapi_browser/stress_many_nexes/nacl.scons',
     'tests/ppapi_example_2d/nacl.scons',
     'tests/ppapi_example_audio/nacl.scons',
     'tests/ppapi_example_events/nacl.scons',

tests/ppapi_browser/stress_many_nexes/nacl.scons
@@ -1,0 +1,31 @@
+# -*- python -*-
+# Copyright (c) 2011 The Native Client Authors.  All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+
+Import('env')
+
+stress_files = [
+    env.File('stress_many_nexes.html'),
+    env.File('${SCONSTRUCT_DIR}/tools/browser_tester/browserdata/nacltest.js'),
+    ]
+replicated_files = env.Replicate('${STAGING_DIR}', stress_files)
+env.Alias('all_programs', replicated_files)
+
+core = env.File('${STAGING_DIR}/ppapi_ppb_core_${TARGET_FULLARCH}${PROGSUFFIX}')
+nmf = env.File('${STAGING_DIR}/ppapi_ppb_core.nmf')
+stress_files.extend([core, nmf])
+
+node = env.PPAPIBrowserTester(
+    'stress_many_nexes_test.out',
+    url='stress_many_nexes.html',
+    timeout=200,
+    files=stress_files)
+
+env.AddNodeToTestSuite(node,
+                       ['chrome_browser_tests'],
+                       'run_stress_many_nexes_test',
+                       # Don't run this test automatically. It can cause random
+                       # processes to die on a machine, etc.
+                       is_broken=True)

tests/ppapi_browser/stress_many_nexes/stress_many_nexes.html
@@ -1,0 +1,88 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+  <!--
+  Copyright (c) 2011 The Native Client Authors. All rights reserved.
+  Use of this source code is governed by a BSD-style license that can be
+  found in the LICENSE file.
+  -->
+  <head>
+    <META HTTP-EQUIV="Pragma" CONTENT="no-cache" />
+    <META HTTP-EQUIV="Expires" CONTENT="-1" />
+    <script type="text/javascript" src="nacltest.js"> </script>
+    <title>Browser Stress Test</title>
+    <style type="text/css">
+      .naclModule { background-color: gray; margin: 2px 2px; }
+      .naclPassed { background-color: green; margin: 2px 2px; }
+      .naclFailed { background-color: red; margin: 2px 2px; }
+    </style>
+  </head>
+  <body id="body">
+<script type="text/javascript">
+//<![CDATA[
+
+function createEmbed(id) {
+  return createNaClEmbed({
+    id: id,
+    src: 'ppapi_ppb_core.nmf',
+    width: 100,
+    height: 20
+  });
+}
+
+
+function checkEmbed(test, embed) {
+  embed.className = 'naclFailed';
+  test.assertEqual(embed.readyState, 4);
+  test.assertEqual(embed.lastError, '');
+  embed.className = 'naclPassed';
+  test.pass();
+}
+
+
+function addParallelTest(tester, i) {
+  var name = 'nacl_' + i;
+  var embed = createEmbed(name);
+  tester.waitFor(embed);
+  tester.addAsyncTest(name, function(test) {
+    checkEmbed(test, embed);
+  });
+  document.body.appendChild(embed);
+}
+
+
+function addSerialTest(tester, i) {
+  var name = 'nacl_' + i;
+  tester.addAsyncTest(name, function(test){
+    var embed = createEmbed(name);
+    test.expectEvent(embed, 'loadend', function() {
+      checkEmbed(test, embed);
+    });
+    document.body.appendChild(embed);
+  });
+}
+
+
+function initTests(tester) {
+  var count = 100;
+  var serial = true;
+  for (var i = 0; i < count; i++) {
+    if (serial) {
+      // Slower, forces sequential load order.
+      addSerialTest(tester, i);
+    } else {
+      // Faster, lets the browser determine load order and interleaving.
+      addParallelTest(tester, i);
+    }
+  }
+}
+
+
+var tester = new Tester($('body'));
+initTests(tester);
+tester.run();
+
+  //]]>
+  </script>
+  </body>
+</html>

commit 47bcaae7b2ffd4ef89ce212bcd94e35ea8376838

Fixed breakage introduced in r6320.
A refactoring did not take into account a code path for outputting debugging messages.

BUG= none
TEST= ./scons chrome_browser_tests

Review URL: http://codereview.chromium.org/7511020

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@6321 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

tools/browser_tester/browserdata/nacltest.js
@@ -685,14 +685,14 @@ function TestStatus(tester, name, async) {
     this.assert(messages.length > 0, 'Must provide at least one message');
     var local_messages = messages.slice();
     var listener = function(message) {
-      // skip debug messages
       if (message.data.indexOf('@:') == 0) {
+        // skip debug messages
         this_.log('DEBUG: ' + message.data.substr(2));
-        return;
-      }
-      this_.assertEqual(message.data, local_messages.shift());
-      if (callback !== undefined) {
-        callback(message.data);
+      } else {
+        this_.assertEqual(message.data, local_messages.shift());
+        if (callback !== undefined) {
+          callback(message.data);
+        }
       }
       if (local_messages.length == 0) {
         this_.pass();