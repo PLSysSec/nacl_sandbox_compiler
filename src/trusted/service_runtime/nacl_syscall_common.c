PLSysSec/nacl_sandbox_compiler

commit 97f417de8f27474d8b94ce680cc69aed639947e4

Removed NaClMemObj because it is no longer needed and replaced its use with enumerator.
BUG=http://code.google.com/p/nativeclient/issues/detail?id=895
TEST=small_tests

Review URL: https://chromiumcodereview.appspot.com/10831283

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@9463 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

src/trusted/service_runtime/nacl_syscall_common.c
@@ -1013,37 +1013,6 @@ cleanup:
   return retval;
 }
 
-void NaClCommonUtilUpdateAddrMap(struct NaClApp       *nap,
-                                 uintptr_t            sysaddr,
-                                 size_t               nbytes,
-                                 int                  sysprot,
-                                 struct NaClDesc      *backing_desc,
-                                 nacl_off64_t         backing_bytes,
-                                 nacl_off64_t         offset_bytes) {
-  uintptr_t                   usraddr;
-  struct NaClMemObj           *nmop;
-
-  NaClLog(3,
-          ("NaClCommonUtilUpdateAddrMap(0x%08"NACL_PRIxPTR", "
-           "0x%08"NACL_PRIxPTR", "
-           "0x%"NACL_PRIxS", 0x%x, 0x%08"NACL_PRIxPTR", 0x%"NACL_PRIx64", "
-           "0x%"NACL_PRIx64")\n"),
-          (uintptr_t) nap, sysaddr, nbytes,
-          sysprot, (uintptr_t) backing_desc, backing_bytes,
-          offset_bytes);
-  usraddr = NaClSysToUser(nap, sysaddr);
-  nmop = NULL;
-  if (NULL != backing_desc) {
-    nmop = NaClMemObjMake(backing_desc, backing_bytes, offset_bytes);
-  }
-
-  NaClVmmapAddWithOverwrite(&nap->mem_map,
-                            usraddr >> NACL_PAGESHIFT,
-                            nbytes >> NACL_PAGESHIFT,
-                            sysprot,
-                            nmop);
-}
-
 
 int NaClSysCommonAddrRangeContainsExecutablePages_mu(struct NaClApp *nap,
                                                      uintptr_t      usraddr,
@@ -1084,7 +1053,6 @@ int32_t NaClCommonSysMmapIntern(struct NaClApp        *nap,
   uintptr_t                   endaddr;
   uintptr_t                   map_result;
   int                         holding_app_lock;
-  struct NaClMemObj           *nmop;
   struct nacl_abi_stat        stbuf;
   size_t                      alloc_rounded_length;
   nacl_off64_t                file_size;
@@ -1093,7 +1061,6 @@ int32_t NaClCommonSysMmapIntern(struct NaClApp        *nap,
   size_t                      alloc_rounded_file_bytes;
 
   holding_app_lock = 0;
-  nmop = NULL;
   ndp = NULL;
 
   allowed_flags = (NACL_ABI_MAP_FIXED | NACL_ABI_MAP_SHARED
@@ -1357,9 +1324,9 @@ int32_t NaClCommonSysMmapIntern(struct NaClApp        *nap,
   /*
    * Never allow users to say that mmapped pages are executable.  This
    * is primarily for the service runtime's own bookkeeping -- prot is
-   * used in NaClCommonUtilUpdateAddrMap -- since %cs restriction
-   * makes page protection irrelevant, it doesn't matter that on many
-   * systems (w/o NX) PROT_READ implies PROT_EXEC.
+   * used in NaClVmmapAddWithOverwrite -- since %cs restriction makes
+   * page protection irrelevant, it doesn't matter that on many systems
+   * (w/o NX) PROT_READ implies PROT_EXEC.
    */
   prot &= ~NACL_ABI_PROT_EXEC;
 
@@ -1421,18 +1388,24 @@ int32_t NaClCommonSysMmapIntern(struct NaClApp        *nap,
     if (map_result != sysaddr) {
       NaClLog(LOG_FATAL, "system mmap did not honor NACL_ABI_MAP_FIXED\n");
     }
-    if (prot == NACL_ABI_PROT_NONE) {
+    if (ndp == NULL || prot == NACL_ABI_PROT_NONE) {
       /*
        * windows nacl_host_desc implementation requires that PROT_NONE
        * memory be freed using VirtualFree rather than
        * UnmapViewOfFile.  TODO(bsy): remove this ugliness.
        */
-      NaClCommonUtilUpdateAddrMap(nap, sysaddr, length, PROT_NONE,
-                                  NULL, file_size, offset);
+      NaClVmmapAddWithOverwrite(&nap->mem_map,
+                                NaClSysToUser(nap, sysaddr) >> NACL_PAGESHIFT,
+                                length >> NACL_PAGESHIFT,
+                                PROT_NONE,
+                                NACL_VMMAP_ENTRY_ANONYMOUS);
     } else {
       /* record change for file-backed memory */
-      NaClCommonUtilUpdateAddrMap(nap, sysaddr, length, NaClProtMap(prot),
-                                  ndp, file_size, offset);
+      NaClVmmapAddWithOverwrite(&nap->mem_map,
+                                NaClSysToUser(nap, sysaddr) >> NACL_PAGESHIFT,
+                                length >> NACL_PAGESHIFT,
+                                NaClProtMap(prot),
+                                NACL_VMMAP_ENTRY_MAPPED);
     }
   } else {
     map_result = sysaddr;
@@ -1484,9 +1457,6 @@ cleanup:
   }
   if (NULL != ndp) {
     NaClDescUnref(ndp);
-  }
-  if (NaClPtrIsNegErrno(&map_result)) {
-    free(nmop);
   }
 
   /*
@@ -1571,36 +1541,51 @@ static int32_t MunmapInternal(struct NaClApp *nap,
       continue;
     }
     NaClLog(3,
-            ("NaClSysMunmap: addr 0x%08x, nmop 0x%08x\n"),
-            addr, entry->nmop);
-    if (NULL == entry->nmop) {
-      /* anonymous memory; we just decommit it and thus make it inaccessible */
-      if (!VirtualFree((void *) addr,
-                       NACL_MAP_PAGESIZE,
-                       MEM_DECOMMIT)) {
-        int error = GetLastError();
-        NaClLog(LOG_FATAL,
-                ("NaClSysMunmap: Could not VirtualFree MEM_DECOMMIT"
-                 " addr 0x%08x, error %d (0x%x)\n"),
-                addr, error, error);
-      }
-    } else {
-      NaClDescUnmapUnsafe(entry->nmop->ndp, (void *) addr, NACL_MAP_PAGESIZE);
-      /*
-       * Fill the address space hole that we opened with
-       * NaClDescUnmapUnsafe().
-       */
-      if (!VirtualAlloc((void *) addr, NACL_MAP_PAGESIZE, MEM_RESERVE,
-                        PAGE_READWRITE)) {
-        NaClLog(LOG_FATAL, "MunmapInternal: "
-                "failed to fill hole with VirtualAlloc(), error %d\n",
-                GetLastError());
-      }
+            ("NaClSysMunmap: addr 0x%08x, type 0x%x\n"),
+            addr, (int) entry->vmmap_type);
+    switch (entry->vmmap_type) {
+      case NACL_VMMAP_ENTRY_ANONYMOUS:
+        /*
+         * Anonymous memory; we just decommit it and thus
+         * make it inaccessible.
+         */
+        if (!VirtualFree((void *) addr,
+                         NACL_MAP_PAGESIZE,
+                         MEM_DECOMMIT)) {
+          int error = GetLastError();
+          NaClLog(LOG_FATAL,
+                  ("NaClSysMunmap: Could not VirtualFree MEM_DECOMMIT"
+                   " addr 0x%08x, error %d (0x%x)\n"),
+                  addr, error, error);
+        }
+        break;
+      case NACL_VMMAP_ENTRY_MAPPED:
+        if (!UnmapViewOfFile((void *) addr)) {
+          int error = GetLastError();
+          NaClLog(LOG_FATAL,
+                  ("NaClSysMunmap: UnmapViewOfFile failed"
+                   " addr 0x%08x, error %d (0x%x)\n"),
+                  addr, error, error);
+        }
+        /*
+         * Fill the address space hole that we opened
+         * with UnmapViewOfFile().
+         */
+        if (!VirtualAlloc((void *) addr, NACL_MAP_PAGESIZE, MEM_RESERVE,
+                          PAGE_READWRITE)) {
+          NaClLog(LOG_FATAL, "MunmapInternal: "
+                  "failed to fill hole with VirtualAlloc(), error %d\n",
+                  GetLastError());
+        }
+        break;
+      default:
+        NaClLog(LOG_FATAL, "MunmapInternal: invalid vmmap_type\n");
+        break;
     }
     NaClVmmapRemove(&nap->mem_map,
                     NaClSysToUser(nap, (uintptr_t) addr) >> NACL_PAGESHIFT,
                     NACL_PAGES_PER_MAP,
-                    (struct NaClMemObj *) NULL);
+                    NACL_VMMAP_ENTRY_ANONYMOUS);
   }
   return 0;
 }
@@ -1625,7 +1610,7 @@ static int32_t MunmapInternal(struct NaClApp *nap,
   NaClVmmapRemove(&nap->mem_map,
                   NaClSysToUser(nap, (uintptr_t) sysaddr) >> NACL_PAGESHIFT,
                   length >> NACL_PAGESHIFT,
-                  (struct NaClMemObj *) NULL);
+                  NACL_VMMAP_ENTRY_ANONYMOUS);
   return 0;
 }
 #endif

src/trusted/service_runtime/build.scons
@@ -65,7 +65,6 @@ ldr_inputs = [
     'nacl_error_log_hook.c',
     'nacl_globals.c',
     'nacl_kern_services.c',
-    'nacl_memory_object.c',
     'nacl_resource.c',
     'nacl_reverse_quota_interface.c',
     'nacl_signal_common.c',

src/trusted/service_runtime/elf_util.c
@@ -754,7 +754,7 @@ NaClErrorCode NaClElfImageLoadDynamically(struct NaClElfImage *image,
                                   php->p_vaddr >> NACL_PAGESHIFT,
                                   mapping_size >> NACL_PAGESHIFT,
                                   PROT_READ,
-                                  NULL);
+                                  NACL_VMMAP_ENTRY_ANONYMOUS);
       }
     }
   }

src/trusted/service_runtime/nacl_desc_effector_ldr.c
@@ -14,7 +14,6 @@
 #include "native_client/src/trusted/service_runtime/nacl_desc_effector_ldr.h"
 
 #include "native_client/src/trusted/service_runtime/include/bits/mman.h"
-#include "native_client/src/trusted/service_runtime/nacl_memory_object.h"
 #include "native_client/src/trusted/service_runtime/nacl_syscall_common.h"
 
 
@@ -36,6 +35,7 @@ static void NaClDescEffLdrUnmapMemory(struct NaClDescEffector  *vself,
   uintptr_t                   endaddr;
   uintptr_t                   usraddr;
   struct NaClVmmapEntry const *map_region;
+  enum NaClVmmapEntryType     entry_type;
 
   for (addr = sysaddr, endaddr = sysaddr + nbytes;
        addr < endaddr;
@@ -44,23 +44,38 @@ static void NaClDescEffLdrUnmapMemory(struct NaClDescEffector  *vself,
 
     map_region = NaClVmmapFindPage(&self->nap->mem_map,
                                    usraddr >> NACL_PAGESHIFT);
-    if (NULL == map_region || NULL == map_region->nmop) {
-      /*
-       * No memory in address space, and we have only MEM_RESERVE'd
-       * the address space; or memory is in address space, but not
-       * backed by a file.
-       */
-      if (0 == VirtualFree((void *) addr, 0, MEM_RELEASE)) {
-        NaClLog(LOG_FATAL,
-                ("NaClMMap: VirtualFree at user addr 0x%08"NACL_PRIxPTR
-                 " (sys 0x%08"NACL_PRIxPTR") failed: windows error %d\n"),
-                usraddr,
-                addr,
-                GetLastError());
-      }
+    if (NULL == map_region) {
+      entry_type = NACL_VMMAP_ENTRY_ANONYMOUS;
     } else {
-      NaClDescUnmapUnsafe(map_region->nmop->ndp,
-                          (void *) addr, NACL_MAP_PAGESIZE);
+      entry_type = map_region->vmmap_type;
+    }
+
+    switch (entry_type) {
+      case NACL_VMMAP_ENTRY_ANONYMOUS:
+        /*
+         * No memory in address space, and we have only MEM_RESERVE'd
+         * the address space; or memory is in address space, but not
+         * backed by a file.
+         */
+        if (!VirtualFree((void *) addr, 0, MEM_RELEASE)) {
+          NaClLog(LOG_FATAL,
+                  ("NaClDescEffLdrUnmapMemory: VirtualFree at user addr"
+                   " 0x%08"NACL_PRIxPTR" (sys 0x%08"NACL_PRIxPTR") failed:"
+                   " error %d\n"),
+                  usraddr, addr, GetLastError());
+        }
+        break;
+      case NACL_VMMAP_ENTRY_MAPPED:
+        if (!UnmapViewOfFile((void *) addr)) {
+          NaClLog(LOG_FATAL,
+                  ("NaClDescEffLdrUnmapMemory: UnmapViewOfFile failed"
+                   " addr 0x%08x, error %d\n"),
+                  addr, GetLastError());
+        }
+        break;
+      default:
+        NaClLog(LOG_FATAL, "NaClDescEffLdrUnmapMemory: invalid vmmap_type\n");
+        break;
     }
   }
 }

src/trusted/service_runtime/nacl_memory_object.c
@@ -1,112 +1,0 @@
-/*
- * Copyright 2008 The Native Client Authors.  All rights reserved.
- * Use of this source code is governed by a BSD-style license that can
- * be found in the LICENSE file.
- */
-
-/*
- * NaCl Simple/secure ELF loader (NaCl SEL) memory object.
- */
-
-#include <stdlib.h>
-
-#include "native_client/src/shared/platform/nacl_log.h"
-#include "native_client/src/shared/platform/nacl_sync_checked.h"
-#include "native_client/src/trusted/service_runtime/nacl_memory_object.h"
-
-
-/*
- * Takes ownership of NaClDesc object, so no manipulation of ref count.
- */
-int NaClMemObjCtor(struct NaClMemObj  *nmop,
-                   struct NaClDesc    *ndp,
-                   nacl_off64_t       nbytes,
-                   nacl_off64_t       offset) {
-  if (NULL == ndp) {
-    NaClLog(LOG_FATAL, "NaClMemObjCtor: ndp is NULL\n");
-  }
-  nmop->ndp = ndp;
-  NaClDescRef(ndp);
-  nmop->nbytes = nbytes;
-  nmop->offset = offset;
-  return 1;
-}
-
-
-/*
- * Placement new copy ctor.
- */
-int NaClMemObjCopyCtorOff(struct NaClMemObj *nmop,
-                          struct NaClMemObj *src,
-                          nacl_off64_t      additional) {
-  nmop->ndp = src->ndp;
-  NaClDescRef(nmop->ndp);
-  nmop->nbytes = src->nbytes;
-  nmop->offset = src->offset + additional;
-  return 1;
-}
-
-
-void NaClMemObjDtor(struct NaClMemObj *nmop) {
-  NaClDescUnref(nmop->ndp);
-  nmop->ndp = NULL;
-  nmop->nbytes = 0;
-  nmop->offset = 0;
-}
-
-void NaClMemObjSafeDtor(struct NaClMemObj *nmop) {
-  if (NULL == nmop) {
-    return;
-  }
-  NaClMemObjDtor(nmop);
-}
-
-
-struct NaClMemObj *NaClMemObjMake(struct NaClDesc *ndp,
-                                  nacl_off64_t    nbytes,
-                                  nacl_off64_t    offset) {
-  struct NaClMemObj *nmop;
-
-  if (NULL == ndp) {
-    NaClLog(4, "NaClMemObjMake: invoked with NULL ndp\n");
-    return NULL;  /* anonymous paging file backed memory */
-  }
-  if (NULL == (nmop = malloc(sizeof *nmop))) {
-    NaClLog(LOG_FATAL, ("NaClMemObjMake: out of memory creating object "
-                        "(NaClDesc = 0x%08"NACL_PRIxPTR", "
-                        "offset = 0x%"NACL_PRIx64")\n"),
-            (uintptr_t) ndp, offset);
-  }
-  if (!NaClMemObjCtor(nmop, ndp, nbytes, offset)) {
-    NaClLog(LOG_FATAL, "NaClMemObjMake: NaClMemObjCtor failed!\n");
-  }
-  return nmop;
-}
-
-
-struct NaClMemObj *NaClMemObjSplit(struct NaClMemObj *orig,
-                                   nacl_off64_t      additional) {
-  struct NaClMemObj *nmop;
-
-  if (NULL == orig)
-    return NULL;
-
-  if (NULL == (nmop = malloc(sizeof *nmop))) {
-    NaClLog(LOG_FATAL, ("NaClMemObjSplit: out of memory creating object"
-                        " (NaClMemObj = 0x%08"NACL_PRIxPTR","
-                        " additional = 0x%"NACL_PRIx64")\n"),
-            (uintptr_t) orig, additional);
-  }
-  if (!NaClMemObjCopyCtorOff(nmop, orig, additional)) {
-    NaClLog(LOG_FATAL, "NaClMemObjSplit: NaClMemObjCopyCtorOff failed\n");
-  }
-  return nmop;
-}
-
-
-void NaClMemObjIncOffset(struct NaClMemObj *nmop,
-                         nacl_off64_t      additional) {
-  if (NULL != nmop) {
-    nmop->offset += additional;
-  }
-}

src/trusted/service_runtime/nacl_memory_object.h
@@ -1,108 +1,0 @@
-/*
- * Copyright 2008 The Native Client Authors.  All rights reserved.
- * Use of this source code is governed by a BSD-style license that can
- * be found in the LICENSE file.
- */
-
-/*
- * NaCl Simple/secure ELF loader (NaCl SEL) memory object.
- */
-
-#ifndef NATIVE_CLIENT_SERVICE_RUNTIME_MEMORY_OBJECT_H_
-#define NATIVE_CLIENT_SERVICE_RUNTIME_MEMORY_OBJECT_H_
-
-#include "native_client/src/include/portability.h"
-
-#include "native_client/src/shared/imc/nacl_imc_c.h"
-#include "native_client/src/shared/platform/nacl_host_desc.h"
-#include "native_client/src/shared/platform/nacl_sync.h"
-#include "native_client/src/trusted/desc/nacl_desc_base.h"
-
-EXTERN_C_BEGIN
-
-/*
- * Memory object for the virtual memory map.  We map in 64KB chunks,
- * and we need this so that we can recreate the memory from the
- * original source should we need to move the address space.
- *
- * This also means that once a descriptor / handle is used to map in
- * memory, we cannot close it unless we know how to recreate it.
- * Thus, the NaClDesc objects are reference counted.
- *
- * Any splitting of NaClMemObj objects should be done with the VM map
- * lock held, so these NaClMemObj objects do not themselves contain a
- * lock.
- */
-
-struct NaClMemObj {
-  struct NaClDesc *ndp;
-  /*
-   * The size of the memory object when the mapping took place.  This
-   * is important becaues the last page in the mapping has to be
-   * handled especially:
-   *
-   * If the object ends in the middle of an allocation, the kernel
-   * will zero pad the allocation.  The zero bytes are writable, but
-   * are not reflected back into the file.  First, assume that the
-   * file size does not change.  This means that for a shared mapping,
-   * when an address space move occurs we have to re-map the file at
-   * the new position, and then copy the contents of the rest of the
-   * allocation to the new location.
-   *
-   * If the file has shrunk, then the shared portion between the new
-   * end of file and the original end of file is impossible to
-   * duplicate as a shared mapping -- we may need to just crash and
-   * burn at that point.
-   *
-   * If the file has grown, then the unshared portion between the old
-   * end-of-file and end of that page (assuming it has grown past the
-   * page boundary) probably should not get overwritten -- and we may
-   * need to crash and burn at this point as well.
-   *
-   * On linux, we could use mremap, but then there's no need: the race
-   * condition with not being able to atomically update the memory
-   * mappings doesn't exist.
-   *
-   * TODO(bsy): figure out exactly what we need to do.
-   */
-  nacl_off64_t           nbytes;  /* stat member is off_t st_size */
-  /*
-   * All memory objects that back virtual memory must support
-   * offset/nbytes mapping.  NB: the VM map code may split a
-   * NaClMemObj into two if some middle page got unmapped, etc.
-   */
-  nacl_off64_t           offset;
-};
-
-/*
- * base class ctor and dtor.  NULL != ndp must hold.
- */
-int NaClMemObjCtor(struct NaClMemObj  *nmop,
-                   struct NaClDesc    *ndp,
-                   nacl_off64_t       nbytes,
-                   nacl_off64_t       offset) NACL_WUR;
-
-int NaClMemObjCopyCtorOff(struct NaClMemObj *nmop,
-                          struct NaClMemObj *src,
-                          nacl_off64_t      additional) NACL_WUR;
-
-void NaClMemObjDtor(struct NaClMemObj *nmop);
-
-void NaClMemObjSafeDtor(struct NaClMemObj *nmop);
-
-/*
- * Allocating Ctor.  Increments refcount on ndp, which must not be NULL.
- */
-struct NaClMemObj *NaClMemObjMake(struct NaClDesc *ndp,
-                                  nacl_off64_t    nbytes,
-                                  nacl_off64_t    offset) NACL_WUR;
-
-struct NaClMemObj *NaClMemObjSplit(struct NaClMemObj  *nmop,
-                                   nacl_off64_t       additional) NACL_WUR;
-
-void NaClMemObjIncOffset(struct NaClMemObj  *nmop,
-                         nacl_off64_t       additional);
-
-EXTERN_C_END
-
-#endif

src/trusted/service_runtime/nacl_syscall_common.h
@@ -104,14 +104,6 @@ int32_t NaClCommonSysStat(struct NaClAppThread *natp,
                           const char           *path,
                           struct nacl_abi_stat *nasp);
 
-void NaClCommonUtilUpdateAddrMap(struct NaClApp   *nap,
-                                 uintptr_t        sysaddr,
-                                 size_t           nbytes,
-                                 int              sysprot,
-                                 struct NaClDesc  *backing_desc,
-                                 nacl_off64_t     backing_bytes,
-                                 nacl_off64_t     offset_bytes);
-
 /* bool */
 int NaClSysCommonAddrRangeContainsExecutablePages_mu(struct NaClApp *nap,
                                                      uintptr_t      usraddr,

src/trusted/service_runtime/posix/nacl_syscall_impl.c
@@ -37,7 +37,6 @@
 #include "native_client/src/trusted/service_runtime/nacl_config.h"
 #include "native_client/src/trusted/service_runtime/nacl_copy.h"
 #include "native_client/src/trusted/service_runtime/nacl_globals.h"
-#include "native_client/src/trusted/service_runtime/nacl_memory_object.h"
 #include "native_client/src/trusted/service_runtime/nacl_syscall_handlers.h"
 #include "native_client/src/trusted/service_runtime/nacl_syscall_common.h"
 #include "native_client/src/trusted/service_runtime/nacl_text.h"

commit 3c4d4f251028d1cc542d867a45ff79154cf4b0eb

The protection for anonymous mapping is stored incorrectly.
This bug was introduced in r9463, discovered by cross-checking the
internal NaCl virtual memory mapping with OS.  The cross-checking test
will be added in later change.

BUG=http://code.google.com/p/nativeclient/issues/detail?id=895
TEST=small_tests

Review URL: https://chromiumcodereview.appspot.com/10823321

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@9486 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

src/trusted/service_runtime/nacl_syscall_common.c
@@ -1343,6 +1343,8 @@ int32_t NaClCommonSysMmapIntern(struct NaClApp        *nap,
 
   /* [0, length) */
   if (length > 0) {
+    enum NaClVmmapEntryType vmmap_type;
+
     if (NULL == ndp) {
       NaClLog(4,
               ("NaClSysMmap: NaClDescIoDescMap(,,0x%08"NACL_PRIxPTR","
@@ -1388,25 +1390,19 @@ int32_t NaClCommonSysMmapIntern(struct NaClApp        *nap,
     if (map_result != sysaddr) {
       NaClLog(LOG_FATAL, "system mmap did not honor NACL_ABI_MAP_FIXED\n");
     }
-    if (ndp == NULL || prot == NACL_ABI_PROT_NONE) {
-      /*
-       * windows nacl_host_desc implementation requires that PROT_NONE
-       * memory be freed using VirtualFree rather than
-       * UnmapViewOfFile.  TODO(bsy): remove this ugliness.
-       */
-      NaClVmmapAddWithOverwrite(&nap->mem_map,
-                                NaClSysToUser(nap, sysaddr) >> NACL_PAGESHIFT,
-                                length >> NACL_PAGESHIFT,
-                                PROT_NONE,
-                                NACL_VMMAP_ENTRY_ANONYMOUS);
-    } else {
-      /* record change for file-backed memory */
-      NaClVmmapAddWithOverwrite(&nap->mem_map,
-                                NaClSysToUser(nap, sysaddr) >> NACL_PAGESHIFT,
-                                length >> NACL_PAGESHIFT,
-                                NaClProtMap(prot),
-                                NACL_VMMAP_ENTRY_MAPPED);
-    }
+    /*
+     * windows nacl_host_desc implementation requires that PROT_NONE
+     * memory be freed using VirtualFree rather than
+     * UnmapViewOfFile.  TODO(bsy): remove this ugliness.
+     */
+    vmmap_type = (NULL == ndp || NACL_ABI_PROT_NONE == prot) ?
+                 NACL_VMMAP_ENTRY_ANONYMOUS :
+                 NACL_VMMAP_ENTRY_MAPPED;
+    NaClVmmapAddWithOverwrite(&nap->mem_map,
+                              NaClSysToUser(nap, sysaddr) >> NACL_PAGESHIFT,
+                              length >> NACL_PAGESHIFT,
+                              NaClProtMap(prot),
+                              vmmap_type);
   } else {
     map_result = sysaddr;
   }