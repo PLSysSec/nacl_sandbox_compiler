PLSysSec/nacl_sandbox_compiler

commit 35d59cf18c1a46b950a52829538820b4f1d9baa2

Report to debugger that NaCl module has killed by signal 9, if it is terminated by closing tab or deleting embed.
BUG= 157881
TEST= gdb_tests
Review URL: https://codereview.chromium.org/11262031

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@10111 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

src/trusted/service_runtime/nacl_signal.h
@@ -45,8 +45,19 @@ enum NaClSignalResult {
   NACL_SIGNAL_RETURN    /* Skip all other handlers and return */
 };
 
+/*
+ * TODO(halyavin): this is part of external ABI. We need to find a directory to
+ * place such headers.
+ */
 #define NACL_ABI_SIGTRAP 5
+#define NACL_ABI_SIGKILL 9
 #define NACL_ABI_SIGSEGV 11
+
+#define NACL_ABI_WEXITSTATUS(status) (((status) & 0xff00) >> 8)
+#define NACL_ABI_WTERMSIG(status) ((status) & 0x7f)
+#define NACL_ABI_WIFEXITED(status) (NACL_ABI_WTERMSIG(status) == 0)
+#define NACL_ABI_WIFSIGNALED(status) ((((status) + 1) & 0x7f) > 1)
+#define NACL_ABI_W_EXITCODE(ret, sig) ((((ret) & 0xff) << 8) + ((sig) & 0x7f))
 
 #if NACL_WINDOWS
 enum PosixSignals {

src/trusted/service_runtime/sel_ldr.h
@@ -77,7 +77,7 @@ struct NaClValidationCache;
 struct NaClDebugCallbacks {
   void (*thread_create_hook)(struct NaClAppThread *natp);
   void (*thread_exit_hook)(struct NaClAppThread *natp);
-  void (*process_exit_hook)(int exit_status);
+  void (*process_exit_hook)();
 };
 
 enum NaClResourcePhase {
@@ -508,6 +508,12 @@ int NaClAppLaunchServiceThreads(struct NaClApp *nap);
  * channel, and false (0) otherwise.
  */
 int NaClReportExitStatus(struct NaClApp *nap, int exit_status);
+
+/*
+ * Report that we are killed with a signal to debug stub and announce
+ * that nexe should be considered no longer running.
+ */
+void NaClReportKilledBySignal(struct NaClApp *nap, int signal);
 
 /*
  * Used to launch the main thread.  NB: calling thread may in the

src/trusted/debug_stub/nacl_debug.cc
@@ -112,8 +112,8 @@ static void ThreadExitHook(struct NaClAppThread *natp) throw() {
   g_target->IgnoreThread(natp);
 }
 
-static void ProcessExitHook(int ErrCode) throw() {
-  g_target->Exit(ErrCode);
+static void ProcessExitHook() throw() {
+  g_target->Exit();
   NaClDebugStubFini();
 }
 

src/trusted/debug_stub/target.cc
@@ -829,12 +829,17 @@ void Target::IgnoreThread(struct NaClAppThread *natp) {
   threads_.erase(iter);
 }
 
-void Target::Exit(int err_code) {
+void Target::Exit() {
   MutexLock lock(&mutex_);
   if (session_ != NULL) {
     Packet exit_packet;
-    exit_packet.AddRawChar('W');
-    exit_packet.AddWord8(err_code);
+    if (NACL_ABI_WIFSIGNALED(nap_->exit_status)) {
+      exit_packet.AddRawChar('X');
+      exit_packet.AddWord8(NACL_ABI_WTERMSIG(nap_->exit_status));
+    } else {
+      exit_packet.AddRawChar('W');
+      exit_packet.AddWord8(NACL_ABI_WEXITSTATUS(nap_->exit_status));
+    }
     session_->SendPacket(&exit_packet);
   }
 }

src/trusted/debug_stub/target.h
@@ -77,7 +77,7 @@ class Target {
   void IgnoreThread(struct NaClAppThread *natp);
 
   // Send exit packet to gdb.
-  void Exit(int err_code);
+  void Exit();
 
  protected:
   uint64_t AdjustUserAddr(uint64_t addr);

src/trusted/service_runtime/nacl_app_thread.c
@@ -129,7 +129,7 @@ void NaClAppThreadTeardown(struct NaClAppThread *natp) {
   NaClLog(3, " NaClThreadExit\n");
 
   NaClXMutexLock(&nap->mu);
-  process_exit_status = nap->exit_status;
+  process_exit_status = NACL_ABI_WEXITSTATUS(nap->exit_status);
   NaClXMutexUnlock(&nap->mu);
   /*
    * There appears to be a race on Windows where the process can sometimes

src/trusted/service_runtime/nacl_secure_service.c
@@ -14,6 +14,7 @@
 
 #include "native_client/src/trusted/fault_injection/fault_injection.h"
 #include "native_client/src/trusted/simple_service/nacl_simple_service.h"
+#include "native_client/src/trusted/service_runtime/nacl_signal.h"
 #include "native_client/src/trusted/service_runtime/sel_ldr.h"
 
 
@@ -101,7 +102,11 @@ static void NaClSecureServiceConnectionCountDecr(
   }
   if (0 == --self->conn_count) {
     NaClLog(4, "NaClSecureServiceThread: all channels closed, exiting.\n");
-    NaClExit(0);
+    /*
+     * Set that we are killed by SIGKILL so that debug stub could report
+     * this to debugger.
+     */
+    NaClReportExitStatus(self->nap, NACL_ABI_W_EXITCODE(0, NACL_ABI_SIGKILL));
   }
   NaClXMutexUnlock(&self->mu);
 }

src/trusted/service_runtime/nacl_syscall_common.c
@@ -45,6 +45,7 @@
 #include "native_client/src/trusted/service_runtime/nacl_app_thread.h"
 #include "native_client/src/trusted/service_runtime/nacl_copy.h"
 #include "native_client/src/trusted/service_runtime/nacl_globals.h"
+#include "native_client/src/trusted/service_runtime/nacl_signal.h"
 #include "native_client/src/trusted/service_runtime/nacl_syscall_handlers.h"
 #include "native_client/src/trusted/service_runtime/nacl_thread_nice.h"
 #include "native_client/src/trusted/service_runtime/nacl_tls.h"
@@ -357,7 +358,7 @@ int32_t NaClCommonSysExit(struct NaClAppThread  *natp,
 
   NaClLog(1, "Exit syscall handler: %d\n", status);
 
-  (void) NaClReportExitStatus(nap, status);
+  (void) NaClReportExitStatus(nap, NACL_ABI_W_EXITCODE(status, 0));
 
   NaClAppThreadTeardown(natp);
   /* NOTREACHED */

commit 03036c374c40062833e990f2fc4e38841870a32d

Fix minor problems introduced by r10111
 * Ensure that process_exit_hook() is defined to take no arguments,
   rather than leaving its arguments unspecified.

 * Remove a function prototype for a non-existent function.

 * Clarify a comment.

BUG=http://code.google.com/p/chromium/issues/detail?id=157881
TEST=build

Review URL: https://codereview.chromium.org/11349002

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@10124 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

src/trusted/service_runtime/nacl_signal.h
@@ -46,8 +46,9 @@ enum NaClSignalResult {
 };
 
 /*
- * TODO(halyavin): this is part of external ABI. We need to find a directory to
- * place such headers.
+ * TODO(halyavin): These signal numbers are part of an external ABI
+ * exposed through NaCl's GDB debug stub.  We need to find a directory
+ * to place such headers.
  */
 #define NACL_ABI_SIGTRAP 5
 #define NACL_ABI_SIGKILL 9

src/trusted/service_runtime/sel_ldr.h
@@ -77,7 +77,7 @@ struct NaClValidationCache;
 struct NaClDebugCallbacks {
   void (*thread_create_hook)(struct NaClAppThread *natp);
   void (*thread_exit_hook)(struct NaClAppThread *natp);
-  void (*process_exit_hook)();
+  void (*process_exit_hook)(void);
 };
 
 enum NaClResourcePhase {
@@ -508,12 +508,6 @@ int NaClAppLaunchServiceThreads(struct NaClApp *nap);
  * channel, and false (0) otherwise.
  */
 int NaClReportExitStatus(struct NaClApp *nap, int exit_status);
-
-/*
- * Report that we are killed with a signal to debug stub and announce
- * that nexe should be considered no longer running.
- */
-void NaClReportKilledBySignal(struct NaClApp *nap, int signal);
 
 /*
  * Used to launch the main thread.  NB: calling thread may in the

tests/signal_handler_single_step/step_test_host.c
@@ -96,8 +96,7 @@ static void ThreadExitHook(struct NaClAppThread *natp) {
   UNREFERENCED_PARAMETER(natp);
 }
 
-static void ProcessExitHook(int exit_status) {
-  UNREFERENCED_PARAMETER(exit_status);
+static void ProcessExitHook(void) {
 }
 
 static const struct NaClDebugCallbacks debug_callbacks = {