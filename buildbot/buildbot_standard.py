PLSysSec/nacl_sandbox_compiler

commit 8f0e67e718e73915881e5e8479cb6d76c2e8cd97

Mac x86-64: Enable more tests on the buildbots/trybots
Mark Mentovai's recent changes made these pass.

BUG=https://code.google.com/p/nativeclient/issues/detail?id=3087
TEST=trybots

Review URL: https://codereview.chromium.org/12259009

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@10788 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

buildbot/buildbot_standard.py
@@ -335,11 +335,19 @@ def BuildScript(status, context):
   # http://code.google.com/p/nativeclient/issues/detail?id=2473
   bug2473 = (context['clang'] or context['asan']) and context['mode'] == 'opt'
   if context.Mac() and context['arch'] != 'arm' and not bug2473:
-    # x86-64 is not fully supported on Mac.  Not everything works, but we
-    # want to stop x86-64 sel_ldr from regressing, so do a minimal test here.
+    # We don't run all tests on x86-64 Mac yet, because not all pass
+    # yet, and because it would slow down the bots too much.  We just
+    # run a small set of tests that have previously been fixed to
+    # pass, in order to prevent regressions.
+    # TODO(mseaborn): Remove this when we have bots dedicated to
+    # testing x86-64 Mac.
     with Step('minimal x86-64 test', status, halt_on_fail=False):
       SCons(context, parallel=True, platform='x86-64',
-            args=['run_hello_world_test'])
+            args=['run_hello_world_test',
+                  'run_execute_data_test',
+                  'run_nacl_signal_test'
+                  'run_signal_frame_test',
+                  'run_trusted_mmap_test'])
 
   ### END tests ###
 

commit 02005c5a2a107c8e9e09c6f02f0c3e948e971b55

Fix build breakage introduced by r10788: Add missing comma in list
I sorted the list before committing and neglected to re-test fully.

BUG=https://code.google.com/p/nativeclient/issues/detail?id=3087
TEST=none

Review URL: https://codereview.chromium.org/12253012

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@10789 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

buildbot/buildbot_standard.py
@@ -345,7 +345,7 @@ def BuildScript(status, context):
       SCons(context, parallel=True, platform='x86-64',
             args=['run_hello_world_test',
                   'run_execute_data_test',
-                  'run_nacl_signal_test'
+                  'run_nacl_signal_test',
                   'run_signal_frame_test',
                   'run_trusted_mmap_test'])
 