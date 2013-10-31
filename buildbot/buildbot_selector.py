PLSysSec/nacl_sandbox_compiler

commit c78b0f1eba5dd4eef0ebab1931f1a4edc46bb89a

Port PNaCl x86 main waterfall buildbots to buildbot_lib
This is part of the effort to move to the toolchain_build.py
infrastructure for PNaCl (and eliminate shell scripts in general), but
it's an explicit non-goal for now to merge them.

Currently buildbot_pnacl.py is used for both main waterfall and
toolchain waterfall bots. I would like to move away from that, and that
may mean factoring some of the test-running code into a common library
in the future, so that the toolchain bots can also run it.

R=jvoung@chromium.org, ncbray@chromium.org
BUG= https://code.google.com/p/nativeclient/issues/detail?id=3719

Review URL: https://codereview.chromium.org/45883002

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@12334 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

buildbot/buildbot_selector.py
@@ -57,9 +57,9 @@ BOT_ASSIGNMENT = {
     'oneiric_32-newlib-arm_hw-pnacl-panda-opt':
         'bash buildbot/buildbot_pnacl.sh mode-buildbot-arm-hw-opt',
     'lucid_64-newlib-x86_32-pnacl':
-        'bash buildbot/buildbot_pnacl.sh mode-buildbot-x86 32',
+      python + ' buildbot/buildbot_pnacl.py opt 32 pnacl',
     'lucid_64-newlib-x86_64-pnacl':
-        'bash buildbot/buildbot_pnacl.sh mode-buildbot-x86 64',
+      python + ' buildbot/buildbot_pnacl.py opt 64 pnacl',
     'lucid_64-newlib-mips-pnacl':
         'echo "TODO(mseaborn): add mips"',
     'precise_64-newlib-arm_qemu-pnacl-dbg':
@@ -153,13 +153,11 @@ BOT_ASSIGNMENT = {
     'nacl-precise_64-newlib-arm_qemu-pnacl':
         'bash buildbot/buildbot_pnacl.sh mode-trybot-qemu',
     'nacl-precise_64-newlib-x86_32-pnacl':
-        'bash buildbot/buildbot_pnacl.sh mode-buildbot-x86 32',
+         python + ' buildbot/buildbot_pnacl.py opt 32 pnacl',
     'nacl-precise_64-newlib-x86_64-pnacl':
-        'bash buildbot/buildbot_pnacl.sh mode-buildbot-x86 64',
+         python + ' buildbot/buildbot_pnacl.py opt 64 pnacl',
     'nacl-precise_64-newlib-mips-pnacl':
         'echo "TODO(mseaborn): add mips"',
-    'nacl-precise_64-newlib-x86_64-pnacl':
-        'bash buildbot/buildbot_pnacl.sh mode-buildbot-x86 64',
     'nacl-arm_opt_panda':
         'bash buildbot/buildbot_pnacl.sh mode-buildbot-arm-try',
     'nacl-arm_hw_opt_panda':

PRESUBMIT.py
@@ -167,7 +167,6 @@ def GetPreferredTrySlaves(_, change):
       'nacl-precise_64-newlib-arm_qemu-pnacl',
       'nacl-precise_64-newlib-x86_32-pnacl',
       'nacl-precise_64-newlib-x86_64-pnacl',
-      'nacl-precise_64-newlib-x86_64-pnacl',
       # pnacl spec2k bots
       'nacl-arm_perf_panda',
       'nacl-precise_64-newlib-x86_32-pnacl-spec',

buildbot/buildbot_lib.py
@@ -89,7 +89,7 @@ def ParseStandardCommandLine(context):
   if arch not in ARCH_MAP:
     parser.error('Invalid arch %r' % arch)
 
-  if clib not in ('newlib', 'glibc'):
+  if clib not in ('newlib', 'glibc', 'pnacl'):
     parser.error('Invalid clib %r' % clib)
 
   # TODO(ncbray) allow a command-line override
@@ -120,6 +120,7 @@ def ParseStandardCommandLine(context):
   if arch != 'arm' or platform == 'linux':
     context['default_scons_mode'] += [mode + '-host']
   context['use_glibc'] = clib == 'glibc'
+  context['pnacl'] = clib == 'pnacl'
   context['max_jobs'] = 8
   context['dry_run'] = options.dry_run
   context['inside_toolchain'] = options.inside_toolchain
@@ -235,6 +236,28 @@ def FileCanBeFound(name, paths):
   return False
 
 
+def RemoveGypBuildDirectories():
+  # Remove all directories on all platforms.  Overkill, but it allows for
+  # straight-line code.
+  # Windows
+  RemoveDirectory('build/Debug')
+  RemoveDirectory('build/Release')
+  RemoveDirectory('build/Debug-Win32')
+  RemoveDirectory('build/Release-Win32')
+  RemoveDirectory('build/Debug-x64')
+  RemoveDirectory('build/Release-x64')
+
+  # Linux and Mac
+  RemoveDirectory('../xcodebuild')
+  RemoveDirectory('../out')
+  RemoveDirectory('src/third_party/nacl_sdk/arm-newlib')
+
+
+def RemoveSconsBuildDirectories():
+  RemoveDirectory('scons-out')
+  RemoveDirectory('breakpad-out')
+
+
 # Execute a command using Python's subprocess module.
 def Command(context, cmd, cwd=None):
   print 'Running command: %s' % ' '.join(cmd)
@@ -304,6 +327,7 @@ def SCons(context, mode=None, platform=None, parallel=False, browser_test=False,
   if context['clang']: cmd.append('--clang')
   if context['asan']: cmd.append('--asan')
   if context['use_glibc']: cmd.append('--nacl_glibc')
+  if context['pnacl']: cmd.append('bitcode=1')
   if context['use_breakpad_tools']:
     cmd.append('breakpad_tools_dir=breakpad-out')
   # Append used-specified arguments.

buildbot/buildbot_pnacl.py
@@ -1,0 +1,89 @@
+#!/usr/bin/python
+# Copyright (c) 2013 The Native Client Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import re
+
+from buildbot_lib import (
+    BuildContext, BuildStatus, Command, ParseStandardCommandLine,
+    RemoveSconsBuildDirectories, RemoveGypBuildDirectories, RunBuild, SCons,
+    Step )
+
+
+def SetupGypDefines(context, extra_vars=[]):
+  context.SetEnv('GYP_DEFINES', ' '.join(context['gyp_vars'] + extra_vars))
+
+
+def SetupLinuxEnvironment(context):
+  SetupGypDefines(context, ['target_arch=' + context['gyp_arch']])
+
+
+def BuildScriptX86(status, context):
+  # Clean out build directories.
+  with Step('clobber', status):
+    RemoveSconsBuildDirectories()
+    RemoveGypBuildDirectories()
+
+  # Unlike their arm counterparts we do not run trusted tests on x86 bots.
+  # Trusted tests get plenty of coverage by other bots, e.g. nacl-gcc bots.
+  # We make the assumption here that there are no "exotic tests" which
+  # are trusted in nature but are somehow depedent on the untrusted TC.
+  flags_build = ['skip_trusted_tests=1', 'do_not_run_tests=1']
+  flags_run = ['skip_trusted_tests=1']
+  smoke_tests = ['small_tests', 'medium_tests']
+
+  with Step('build_all', status):
+    SCons(context, parallel=True, args=flags_build)
+
+  # Normal pexe-mode tests
+  with Step('smoke_tests', status, halt_on_fail=False):
+    SCons(context, parallel=True, args=flags_run + smoke_tests)
+  # Large tests cannot be run in parallel
+  with Step('large_tests', status, halt_on_fail=False):
+    SCons(context, parallel=False, args=flags_run + ['large_tests'])
+
+  # non-pexe-mode tests. Build everything to make sure it all builds in nonpexe
+  # mode, but just run the nonpexe_tests
+  with Step('build_nonpexe', status):
+    SCons(context, parallel=True, args=flags_build + ['pnacl_generate_pexe=0'])
+  with Step('nonpexe_tests', status, halt_on_fail=False):
+    SCons(context, parallel=True,
+          args=flags_run + ['pnacl_generate_pexe=0', 'nonpexe_tests'])
+
+  irt_mode = context['default_scons_mode'] + ['nacl_irt_test']
+  smoke_tests_irt = ['small_tests_irt', 'medium_tests_irt']
+  # Run some tests with the IRT
+  with Step('smoke_tests_irt', status, halt_on_fail=False):
+    SCons(context, parallel=True, mode=irt_mode,
+          args=flags_run + smoke_tests_irt)
+
+  # Test sandboxed translation
+  with Step('smoke_tests_sandboxed_translator', status):
+    SCons(context, parallel=True, mode=irt_mode,
+          args=flags_run + ['use_sandboxed_translator=1'] + smoke_tests_irt)
+  with Step('smoke_tests_sandboxed_fast', status):
+    SCons(context, parallel=True, mode=irt_mode,
+          args=flags_run + ['use_sandboxed_translator=1', 'translate_fast=1']
+          + smoke_tests_irt)
+
+  # Translator memory consumption regression test
+  with Step('large_code_test', status):
+    SCons(context, parallel=True, mode=irt_mode,
+          args=flags_run + ['use_sandboxed_translator=1', 'large_code'])
+
+
+def Main():
+  context = BuildContext()
+  status = BuildStatus(context)
+  ParseStandardCommandLine(context)
+
+  if context.Linux():
+    SetupLinuxEnvironment(context)
+  else:
+    raise Exception('Unsupported platform')
+
+  RunBuild(BuildScriptX86, status)
+
+if __name__ == '__main__':
+  Main()

buildbot/buildbot_pnacl.sh
@@ -373,55 +373,6 @@ llvm-regression() {
   ${LLVM_TEST} --llvm-regression --check-excludes || handle-error
 }
 
-# This function is shared between x86-32 and x86-64. All building and testing
-# is done on the same bot. Try runs are identical to buildbot runs
-#
-# Note: unlike its arm counterparts we do not run trusted tests on these bots.
-# Trusted tests get plenty of coverage by other bots, e.g. nacl-gcc bots.
-# We make the assumption here that there are no "exotic tests" which
-# are trusted in nature but are somehow depedent on the untrusted TC.
-mode-buildbot-x86() {
-  local arch="x86-$1"
-  FAIL_FAST=false
-  clobber
-
-  local flags_build="skip_trusted_tests=1 -k -j8 do_not_run_tests=1"
-  local flags_run="skip_trusted_tests=1 -k -j4"
-  # Normal pexe tests. Build all targets, then run (-j4 is a compromise to try
-  # to reduce cycle times without making output too difficult to follow)
-  # We also intentionally build more than we run for "coverage".
-  # specifying "" as the target which means "build everything"
-
-  scons-stage-noirt "${arch}" "${flags_build}" "${SCONS_EVERYTHING}"
-  scons-stage-noirt "${arch}" "${flags_run}"   "${SCONS_S_M}"
-  # Large tests cannot be run in parallel
-  scons-stage-noirt "${arch}" "${flags_run} -j1"  "large_tests"
-
-  # non-pexe tests (Do the build-everything step just to make sure it all still
-  # builds as non-pexe)
-  scons-stage-noirt "${arch}" "${flags_build} pnacl_generate_pexe=0" \
-      "${SCONS_EVERYTHING}"
-  scons-stage-noirt "${arch}" "${flags_run} pnacl_generate_pexe=0" \
-      "nonpexe_tests"
-
-  # also run some tests with the irt
-  scons-stage-irt "${arch}" "${flags_run}" "${SCONS_S_M_IRT}"
-
-  # sandboxed translation
-  build-sbtc-prerequisites ${arch}
-  scons-stage-irt "${arch}" "${flags_build} use_sandboxed_translator=1" \
-    "${SCONS_EVERYTHING}"
-  scons-stage-irt "${arch}" "${flags_run} use_sandboxed_translator=1" \
-    "${SCONS_S_M_IRT}"
-  scons-stage-irt "${arch}" \
-    "${flags_run} use_sandboxed_translator=1 translate_fast=1" \
-    "toolchain_tests"
-
-  # translator memory consumption regression test
-  scons-stage-irt "${arch}" "${flags_run} use_sandboxed_translator=1" \
-      "large_code"
-}
-
 # QEMU upload bot runs this function, and the hardware download bot runs
 # mode-buildbot-arm-hw
 mode-buildbot-arm() {

buildbot/buildbot_standard.py
@@ -14,7 +14,8 @@ import time
 
 from buildbot_lib import (
     BuildContext, BuildStatus, Command, EnsureDirectoryExists,
-    ParseStandardCommandLine, RemoveDirectory, RunBuild, SCons, Step, StepLink,
+    ParseStandardCommandLine, RemoveDirectory, RemoveGypBuildDirectories,
+    RemoveSconsBuildDirectories, RunBuild, SCons, Step, StepLink,
     StepText, TryToCleanContents)
 
 
@@ -193,30 +194,12 @@ def CommandGclientRunhooks(context):
   Command(context, cmd=[gclient, 'runhooks', '--force'])
 
 
-def RemoveGypBuildDirectories():
-  # Remove all directories on all platforms.  Overkill, but it allows for
-  # straight-line code.
-  # Windows
-  RemoveDirectory('build/Debug')
-  RemoveDirectory('build/Release')
-  RemoveDirectory('build/Debug-Win32')
-  RemoveDirectory('build/Release-Win32')
-  RemoveDirectory('build/Debug-x64')
-  RemoveDirectory('build/Release-x64')
-
-  # Linux and Mac
-  RemoveDirectory('../xcodebuild')
-  RemoveDirectory('../out')
-  RemoveDirectory('src/third_party/nacl_sdk/arm-newlib')
-
-
 def BuildScript(status, context):
   inside_toolchain = context['inside_toolchain']
 
   # Clean out build directories.
   with Step('clobber', status):
-    RemoveDirectory('scons-out')
-    RemoveDirectory('breakpad-out')
+    RemoveSconsBuildDirectories()
     RemoveGypBuildDirectories()
 
   with Step('cleanup_temp', status):

commit 700394f93fbc51da18509e77419446a61568245e

Fix bot breakage in r12334 and remove obsolete PNaCl bots from buildbot_selector.py
TBR=jvoung@chromium.org
BUG=bots

Review URL: https://codereview.chromium.org/54673006

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@12335 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

buildbot/buildbot_selector.py
@@ -48,28 +48,18 @@ BOT_ASSIGNMENT = {
       python + ' buildbot/buildbot_standard.py opt 32 newlib --asan',
 
     # PNaCl.
-    'lucid_64-newlib-arm_qemu-pnacl-dbg':
-        'bash buildbot/buildbot_pnacl.sh mode-buildbot-arm-dbg',
-    'lucid_64-newlib-arm_qemu-pnacl-opt':
-        'bash buildbot/buildbot_pnacl.sh mode-buildbot-arm-opt',
     'oneiric_32-newlib-arm_hw-pnacl-panda-dbg':
         'bash buildbot/buildbot_pnacl.sh mode-buildbot-arm-hw-dbg',
     'oneiric_32-newlib-arm_hw-pnacl-panda-opt':
         'bash buildbot/buildbot_pnacl.sh mode-buildbot-arm-hw-opt',
-    'lucid_64-newlib-x86_32-pnacl':
-      python + ' buildbot/buildbot_pnacl.py opt 32 pnacl',
-    'lucid_64-newlib-x86_64-pnacl':
-      python + ' buildbot/buildbot_pnacl.py opt 64 pnacl',
-    'lucid_64-newlib-mips-pnacl':
-        'echo "TODO(mseaborn): add mips"',
     'precise_64-newlib-arm_qemu-pnacl-dbg':
         'bash buildbot/buildbot_pnacl.sh mode-buildbot-arm-dbg',
     'precise_64-newlib-arm_qemu-pnacl-opt':
         'bash buildbot/buildbot_pnacl.sh mode-buildbot-arm-opt',
     'precise_64-newlib-x86_32-pnacl':
-        'bash buildbot/buildbot_pnacl.sh mode-buildbot-x86 32',
+      python + ' buildbot/buildbot_pnacl.py opt 32 pnacl',
     'precise_64-newlib-x86_64-pnacl':
-        'bash buildbot/buildbot_pnacl.sh mode-buildbot-x86 64',
+      python + ' buildbot/buildbot_pnacl.py opt 64 pnacl',
     'precise_64-newlib-mips-pnacl':
         'echo "TODO(mseaborn): add mips"',
     # PNaCl Spec