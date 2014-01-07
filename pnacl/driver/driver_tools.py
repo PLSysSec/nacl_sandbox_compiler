PLSysSec/nacl_sandbox_compiler

commit 202325e2221a9f0e03176dd9e5d3f4f1fb0e016a

PNaCl driver: Use paths given by -B for finding host binaries
Currently the compiler driver accepts -B but only uses it to inject
header search paths. This CL also makes it work to inject host binary
(i.e. llvm and binutils) search paths.

This makes it more like gcc's -B flag (it also
makes -B work for pnacl's binutils, whereas the real
binutils do not support -B at all).

This will be used in the new toolchain build script to avoid having
to assemble the toolchain files into one place to build target
libraries.

R=jvoung@chromium.org, stichnot@chromium.org
BUG= https://code.google.com/p/nativeclient/issues/detail?id=3719
TEST= change the driver test harness' host-path override mechanism to use this mechanism

Review URL: https://codereview.chromium.org/120333002

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@12576 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

pnacl/driver/driver_tools.py
@@ -191,6 +191,28 @@ def FindBasePNaCl():
   basedir = pathtools.dirname(bindir)
   return shell.escape(basedir)
 
+def AddHostBinarySearchPath(prefix):
+  """ Add a path to the list searched for host binaries. """
+  prefix = pathtools.normalize(prefix)
+  if pathtools.isdir(prefix) and not prefix.endswith('/'):
+    prefix += '/'
+
+  env.append('BPREFIXES', prefix)
+
+@env.register
+def FindBaseHost(tool):
+  """ Find the base directory for host binaries (i.e. llvm/binutils) """
+  if env.has('BPREFIXES'):
+    for prefix in env.get('BPREFIXES'):
+      if os.path.exists(pathtools.join(prefix, 'bin', tool)):
+        return prefix
+
+  base_pnacl = FindBasePNaCl()
+  base_host = pathtools.join(base_pnacl, 'host_' + env.getone('HOST_ARCH'))
+  if not pathtools.exists(pathtools.join(base_host, 'bin', tool)):
+    Log.Fatal('Could not find PNaCl host directory for ' + tool)
+  return base_host
+
 def ReadConfig():
   # Mock out ReadConfig if running unittests.  Settings are applied directly
   # by DriverTestEnv rather than reading this configuration file.
@@ -226,7 +248,7 @@ def AddPrefix(prefix, varname):
 ######################################################################
 
 DriverArgPatterns = [
-  ( '--pnacl-driver-verbose',             "env.set('LOG_VERBOSE', '1')"),
+  ( '--pnacl-driver-verbose',          "env.set('LOG_VERBOSE', '1')"),
   ( ('-arch', '(.+)'),                 "SetArch($0)"),
   ( '--pnacl-sb',                      "env.set('SANDBOXED', '1')"),
   ( '--pnacl-use-emulator',            "env.set('USE_EMULATOR', '1')"),
@@ -239,6 +261,7 @@ DriverArgPatterns = [
   ( '--pnacl-default-command-line',    "env.set('USE_DEFAULT_CMD_LINE', '1')"),
   ( '-save-temps',                     "env.set('SAVE_TEMPS', '1')"),
   ( '-no-save-temps',                  "env.set('SAVE_TEMPS', '0')"),
+  ( ('-B', '(.*)'),  AddHostBinarySearchPath),
  ]
 
 DriverArgPatternsNotInherited = [

pnacl/driver/driver_env.py
@@ -31,9 +31,9 @@ INITIAL_ENV = {
   'BUILD_ARCH'      : '${@GetBuildArch}',      # "x86_64" or "i686" or "i386"
 
   # Directories
-  'BASE_HOST'       : '${BASE}/host_${HOST_ARCH}',
-  'BASE_LLVM'       : '${BASE_HOST}',
-  'BASE_BINUTILS'   : '${BASE_HOST}',
+  'BPREFIXES'       : '', # Prefixes specified using the -B flag.
+  'BASE_LLVM'       : '${@FindBaseHost:clang}',
+  'BASE_BINUTILS'   : '${@FindBaseHost:le32-nacl-ar}',
 
   'BASE_LIB_NATIVE' : '${BASE}/lib-',
 

pnacl/driver/pnacl-driver.py
@@ -120,9 +120,7 @@ EXTRA_ENV = {
                '${PIC ? -fPIC} ${@AddPrefix:-L:SEARCH_DIRS} ' +
                '--pnacl-exceptions=${CXX_EH_MODE}',
 
-  'SEARCH_DIRS'      : '${SEARCH_DIRS_USER} ${PREFIXES}',
-  'SEARCH_DIRS_USER' : '', # Directories specified using -L
-  'PREFIXES'         : '', # Prefixes specified by using the -B flag.
+  'SEARCH_DIRS' : '', # Directories specified using -L
 
   # Library Strings
   'EMITMODE'         : '${!USE_STDLIB ? nostdlib : static}',
@@ -183,18 +181,6 @@ def AddDiagnosticFlag(*args):
   env.append('CC_FLAGS', *args)
   env.set('DIAGNOSTIC', '1')
 
-def AddBPrefix(prefix):
-  prefix = pathtools.normalize(prefix)
-  if pathtools.isdir(prefix) and not prefix.endswith('/'):
-    prefix += '/'
-
-  env.append('PREFIXES', prefix)
-
-  # Add prefix/include to isystem if it exists
-  include_dir = prefix + 'include'
-  if pathtools.isdir(include_dir):
-    env.append('ISYSTEM_USER', include_dir)
-
 def SetTarget(*args):
   arch = ParseTriple(args[0])
   env.set('FRONTEND_TRIPLE', args[0])
@@ -244,6 +230,22 @@ def HandleDashX(arg):
     filetype.SetForcedFileType(None)
     return
   filetype.SetForcedFileType(filetype.GCCTypeToFileType(arg))
+
+def AddBPrefix(prefix):
+  """ Add a path to the list searched for host binaries and include dirs. """
+  AddHostBinarySearchPath(prefix)
+  prefix = pathtools.normalize(prefix)
+  if pathtools.isdir(prefix) and not prefix.endswith('/'):
+    prefix += '/'
+
+  # Add prefix/ to the library search dir if it exists
+  if pathtools.isdir(prefix):
+    env.append('SEARCH_DIRS', prefix)
+
+  # Add prefix/include to isystem if it exists
+  include_dir = prefix + 'include'
+  if pathtools.isdir(include_dir):
+    env.append('ISYSTEM_USER', include_dir)
 
 CustomPatterns = [
   ( '--driver=(.+)',                "env.set('CC', pathtools.normalize($0))\n"),
@@ -351,8 +353,8 @@ GCCPatterns = [
   ( ('-B','(.*)'),            AddBPrefix),
   ( ('-B(.+)'),               AddBPrefix),
 
-  ( ('-L','(.+)'), "env.append('SEARCH_DIRS_USER', pathtools.normalize($0))"),
-  ( '-L(.+)',      "env.append('SEARCH_DIRS_USER', pathtools.normalize($0))"),
+  ( ('-L','(.+)'), "env.append('SEARCH_DIRS', pathtools.normalize($0))"),
+  ( '-L(.+)',      "env.append('SEARCH_DIRS', pathtools.normalize($0))"),
 
   ( '(-Wp,.*)', AddCCFlag),
   ( '(-Xpreprocessor .*)', AddCCFlag),

pnacl/driver/tests/driver_test_utils.py
@@ -12,6 +12,7 @@ import unittest
 
 import driver_env
 import driver_log
+import driver_tools
 
 def CanRunHost():
   # Some of the test+tools require running the host binaries, but that
@@ -32,7 +33,8 @@ def _SetupLinuxHostDir(env, nacl_dir):
                               'host_%s')
   dir_32 = dir_template % 'x86_32'
   dir_64 = dir_template % 'x86_64'
-  env.set('BASE_HOST', dir_32 if os.path.exists(dir_32) else dir_64)
+  driver_tools.AddHostBinarySearchPath(
+    dir_32 if os.path.exists(dir_32) else dir_64)
 
 def SetupNaClDir(env):
   test_dir = os.path.abspath(dirname(__file__))
@@ -46,8 +48,9 @@ def SetupToolchainDir(env):
   env.set('BASE_TOOLCHAIN', toolchain_dir)
 
 def SetupHostDir(env):
-  # Some of the tools require 'BASE_HOST' to be set, because they end up
-  # running one of the host binaries.
+  # Some of the tools end up running one of the host binaries. Find the host
+  # dir on the test system and inject it into the search path using the
+  # implementation of -B
   test_dir = os.path.abspath(dirname(__file__))
   nacl_dir = dirname(dirname(dirname(test_dir)))
   if sys.platform == 'darwin':
@@ -62,7 +65,7 @@ def SetupHostDir(env):
   host_dir = os.path.join(nacl_dir, 'toolchain',
                           'pnacl_%s_x86' % os_shortname,
                           'host_%s' % host_arch)
-  env.set('BASE_HOST', host_dir)
+  driver_tools.AddHostBinarySearchPath(host_dir)
 
 # A collection of override methods that mock driver_env.Environment.
 

commit 0a788081af8e2826dd8d1acc8907428b805c7598

PNaCl driver: fix host-tools directory check for Windows
The check for the existence of the host tools directory failed to account
for differing file extensions on Windows. This fixes the toolchain build
breakage from r12576.

R=jvoung@chromium.org
BUG=none
TEST=local build

Review URL: https://codereview.chromium.org/125553003

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@12578 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

pnacl/driver/driver_tools.py
@@ -209,7 +209,8 @@ def FindBaseHost(tool):
 
   base_pnacl = FindBasePNaCl()
   base_host = pathtools.join(base_pnacl, 'host_' + env.getone('HOST_ARCH'))
-  if not pathtools.exists(pathtools.join(base_host, 'bin', tool)):
+  if not pathtools.exists(pathtools.join(base_host, 'bin',
+                          tool + env.getone('EXEC_EXT'))):
     Log.Fatal('Could not find PNaCl host directory for ' + tool)
   return base_host
 