PLSysSec/nacl_sandbox_compiler

commit 3fd6ed34deb78c701dfcca973ba19cac73e29617

A mechanism to identify/forbid/"rewrite" non-temporal instructions (and other)
For security reasons, the validator needs to identify certain instructions, then forbid or rewrite them based on further information. In this CL, we target non-temporal instructions, including non-temporal reads, writes, and prefetches.

Specifically:
1) We mark non-temporal instructions in def files with an additional attribute "nacl-unsupported";
2) The validator identifies instructions with "nacl-unsupported" attribute
3) Based on the flags passed in, the validator either forbids or "rewrites" the instruction. Note:
-- the rewriting is to be implemented in a separate CL, and the current behavior is to leave the instruction as it is (i.e., as if it passes validation), which is consistent with the old behavior.
-- the flags are currently derived from the "pnacl_mode" argument, which makes the validator disable non-temporal instructions under PNaCl.

BUG=https://code.google.com/p/chromium/issues/detail?id=500026
R=jfb@chromium.org, mseaborn@chromium.org, phosek@chromium.org

Review URL: https://codereview.chromium.org/1234393005 .

src/trusted/validator_ragel/dfa_validate_common.c
@@ -33,16 +33,26 @@ Bool NaClDfaProcessValidationError(const uint8_t *begin, const uint8_t *end,
   return FALSE;
 }
 
-Bool NaClDfaStubOutCPUUnsupportedInstruction(const uint8_t *begin,
-                                             const uint8_t *end,
-                                             uint32_t info,
-                                             void *callback_data) {
+Bool NaClDfaStubOutUnsupportedInstruction(const uint8_t *begin,
+                                          const uint8_t *end,
+                                          uint32_t info,
+                                          void *callback_data) {
+  struct StubOutCallbackData *data = callback_data;
   /* Stub-out instructions unsupported on this CPU, but valid on other CPUs.  */
   if ((info & VALIDATION_ERRORS_MASK) == CPUID_UNSUPPORTED_INSTRUCTION) {
-    int *did_stubout = callback_data;
-    *did_stubout = 1;
+    data->did_rewrite = 1;
     memset((uint8_t *)begin, NACL_HALT_OPCODE, end - begin);
     return TRUE;
+  } else if ((info & VALIDATION_ERRORS_MASK) == UNSUPPORTED_INSTRUCTION) {
+    if (data->flags == DISABLE_NONTEMPORALS) {
+      return FALSE;
+    } else {
+      /* TODO(ruiq): rewrite instruction. For now, we keep the original
+       * instruction and indicate validation success, which is consistent
+       * with current validation results. */
+      data->did_rewrite = 0;
+      return TRUE;
+    }
   } else {
     return FALSE;
   }

src/trusted/validator_ragel/dfa_validate_common.h
@@ -31,10 +31,16 @@
 Bool NaClDfaProcessValidationError(const uint8_t *begin, const uint8_t *end,
                                    uint32_t info, void *callback_data);
 
-Bool NaClDfaStubOutCPUUnsupportedInstruction(const uint8_t *begin,
-                                             const uint8_t *end,
-                                             uint32_t info,
-                                             void *callback_data);
+struct StubOutCallbackData {
+  uint32_t flags;
+  int did_rewrite;
+};
+
+Bool NaClDfaStubOutUnsupportedInstruction(const uint8_t *begin,
+                                          const uint8_t *end,
+                                          uint32_t info,
+                                          void *callback_data);
+
 struct CodeCopyCallbackData {
   NaClCopyInstructionFunc copy_func;
   /* Difference between addresses: dest - src.  */

src/trusted/validator/validation_cache_test.cc
@@ -168,6 +168,7 @@ class ValidationCachingInterfaceTests : public ::testing::Test {
   NaClValidationStatus Validate() {
     return validator->Validate(0, code_buffer, 32,
                                FALSE,  /* stubout_mode */
+                               0,      /* flags */
                                FALSE,  /* readonly_test */
                                cpu_features,
                                metadata_ptr,
@@ -194,6 +195,7 @@ TEST_F(ValidationCachingInterfaceTests, NoCache) {
   NaClValidationStatus status = validator->Validate(
       0, code_buffer, CODE_SIZE,
       FALSE, /* stubout_mode */
+      0,     /* flags */
       FALSE, /* readonly_test */
       cpu_features,
       NULL, /* metadata */

DEPS
@@ -32,7 +32,7 @@ vars = {
   "mingw_rev": "3cc8b140b883a9fe4986d12cfd46c16a093d3527", # from svn revision 7064
   "nsis_rev": "21b6ad22daa7bfc04b9f1c1805a34622e2607a93", # from svn revision 7071
   "ragel_rev": "da42bb33f1b67c2d70b38ec1d2edf5263271b635", # from svn revision 9010
-  "validator_snapshots_rev": "e74fe4a1a1e5daa97b89b32c1a2a3a3956d9fd7e",
+  "validator_snapshots_rev": "ef053694ef9b0d98d9bed0b9bb649963084bfc81",
 
   "chromium_git": "https://chromium.googlesource.com",
 

src/trusted/service_runtime/elf_util.c
@@ -648,6 +648,7 @@ static NaClErrorCode NaClElfFileMapSegment(struct NaClApp *nap,
                      (uint8_t *) image_sys_addr,
                      segment_size,  /* actual size */
                      0,  /* stubout_mode: no */
+                     nap->pnacl_mode ? DISABLE_NONTEMPORALS : 0,
                      1,  /* readonly_text: yes */
                      nap->cpu_features,
                      &metadata,

src/trusted/service_runtime/sel_validate_image.c
@@ -35,6 +35,7 @@ int NaClValidateCode(struct NaClApp *nap, uintptr_t guest_addr,
   NaClValidationStatus status = NaClValidationSucceeded;
   struct NaClValidationCache *cache = nap->validation_cache;
   const struct NaClValidatorInterface *validator = nap->validator;
+  uint32_t flags = nap->pnacl_mode ? DISABLE_NONTEMPORALS : 0;
 
   if (size < kMinimumCachedCodeSize) {
     /*
@@ -73,6 +74,7 @@ int NaClValidateCode(struct NaClApp *nap, uintptr_t guest_addr,
        allowable HLTs. */
     status = validator->Validate(guest_addr, data, size,
                                  TRUE, /* stub out */
+                                 flags,
                                  FALSE, /* text is not read-only */
                                  nap->cpu_features,
                                  metadata,
@@ -83,6 +85,7 @@ int NaClValidateCode(struct NaClApp *nap, uintptr_t guest_addr,
     int readonly_text = nap->fixed_feature_cpu_mode;
     status = validator->Validate(guest_addr, data, size,
                                  FALSE, /* do not stub out */
+                                 flags,
                                  readonly_text,
                                  nap->cpu_features,
                                  metadata,

src/trusted/service_runtime/sys_memory.c
@@ -267,6 +267,7 @@ int32_t NaClSysMmapIntern(struct NaClApp        *nap,
   nacl_off64_t                file_bytes;
   nacl_off64_t                host_rounded_file_bytes;
   size_t                      alloc_rounded_file_bytes;
+  uint32_t                    val_flags;
 
   holding_app_lock = 0;
   ndp = NULL;
@@ -721,6 +722,7 @@ int32_t NaClSysMmapIntern(struct NaClApp        *nap,
         goto cleanup;
       }
 
+      val_flags = nap->pnacl_mode ? DISABLE_NONTEMPORALS : 0;
       /* Ask validator / validation cache */
       NaClMetadataFromNaClDescCtor(&metadata, ndp);
       validator_status = NACL_FI("MMAP_FORCE_MMAP_VALIDATION_FAIL",
@@ -729,6 +731,7 @@ int32_t NaClSysMmapIntern(struct NaClApp        *nap,
                                             (uint8_t *) image_sys_addr,
                                             length,
                                             0,  /* stubout_mode: no */
+                                            val_flags,
                                             1,  /* readonly_text: yes */
                                             nap->cpu_features,
                                             &metadata,

src/trusted/validator/build.scons
@@ -47,3 +47,18 @@ if env.Bit('build_x86') or env.Bit('build_arm'):
 
   env.AddNodeToTestSuite(node, ['small_tests', 'validator_tests'],
                          'run_validator_debug_stub_test')
+
+if env.Bit('build_x86_64'):
+  gtest_env = env.MakeGTestEnv()
+
+  validation_disable_nontemporals_test_exe = gtest_env.ComponentProgram(
+      'validation_disable_nontemporals_test',
+      ['validation_disable_nontemporals_test.cc'],
+      EXTRA_LIBS=['validators', 'nrd_xfer'])
+
+  node = gtest_env.CommandTest(
+      'validation_disable_nontemporals_test.out',
+      command=[validation_disable_nontemporals_test_exe])
+
+  env.AddNodeToTestSuite(node, ['small_tests', 'validator_tests'],
+                         'run_disable_nontemporals_test')

commit 73b8e1453389f836f0288a536cb73dc8cab3faf5

Validator: Fix minor regression in validation caching's handling of stub-out
Validation judgements are not supposed to be cached if the validator
modifies code (by stubbing out instructions with HLTs).

Change 3fd6ed34deb78c701dfcca973ba19cac73e29617 introduced a minor
regression to that in the case where a non-temporal instruction
appears after stubbed-out instructions.  This would allow the
non-stubbed-out version to be validation-cached, bypassing
stubbing-out.

Fix this by removing the assignment "did_rewrite = 0".

BUG=https://code.google.com/p/chromium/issues/detail?id=500026
TEST=run_validation_cache_test
R=phosek@chromium.org

Review URL: https://codereview.chromium.org/1309393002 .

src/trusted/validator_ragel/dfa_validate_common.c
@@ -50,7 +50,6 @@ Bool NaClDfaStubOutUnsupportedInstruction(const uint8_t *begin,
       /* TODO(ruiq): rewrite instruction. For now, we keep the original
        * instruction and indicate validation success, which is consistent
        * with current validation results. */
-      data->did_rewrite = 0;
       return TRUE;
     }
   } else {

src/trusted/validator_ragel/dfa_validate_common.h
@@ -33,6 +33,12 @@ Bool NaClDfaProcessValidationError(const uint8_t *begin, const uint8_t *end,
 
 struct StubOutCallbackData {
   uint32_t flags;
+  /*
+   * |did_rewrite| records whether the validator modified the code being
+   * validated (e.g. to stub out instructions that aren't supported on this
+   * CPU).  This starts out as FALSE and may be set to TRUE.  It should
+   * never be reset to FALSE.
+   */
   int did_rewrite;
 };
 

src/trusted/validator/validation_cache_test.cc
@@ -46,9 +46,17 @@ const char ret[CODE_SIZE + 1] =
     "\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90";
 #endif
 
-// pblendw $0xc0,%xmm0,%xmm2
+// Example of an instruction which may get "stubbed out" (replaced with
+// HLTs) if the CPU does not support it.
 const char sse41[CODE_SIZE + 1] =
-    "\x66\x0f\x3a\x0e\xd0\xc0\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90"
+    "\x66\x0f\x3a\x0e\xd0\xc0"  // pblendw $0xc0,%xmm0,%xmm2
+    "\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90"
+    "\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90";
+
+const char sse41_plus_nontemporal[CODE_SIZE + 1] =
+    "\x66\x0f\x3a\x0e\xd0\xc0"  // pblendw $0xc0,%xmm0,%xmm2
+    "\x0f\x18\x04\x24"  // prefetchnta (%rsp)
+    "\x90\x90\x90\x90\x90\x90"
     "\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90";
 
 struct MockContext {
@@ -229,7 +237,23 @@ TEST_F(ValidationCachingInterfaceTests, SSE4Allowed) {
 }
 
 TEST_F(ValidationCachingInterfaceTests, SSE4Stubout) {
+  // Check that the validation is not cached if the validator modifies the
+  // the code (by stubbing out instructions).
   memcpy(code_buffer, sse41, CODE_SIZE);
+  context.query_result = 0;
+  /* TODO(jfb) Use a safe cast here, this test should only run for x86. */
+  NaClSetCPUFeatureX86((NaClCPUFeaturesX86 *) cpu_features,
+                       NaClCPUFeatureX86_SSE41, 0);
+  NaClValidationStatus status = Validate();
+  EXPECT_EQ(NaClValidationSucceeded, status);
+  EXPECT_EQ(true, context.query_destroyed);
+}
+
+TEST_F(ValidationCachingInterfaceTests, NonTemporalStubout) {
+  // The validation should not be cached if the validator modifies the
+  // code.  This test checks for a regression where that property was
+  // broken if the code contained a non-temporal instruction.
+  memcpy(code_buffer, sse41_plus_nontemporal, CODE_SIZE);
   context.query_result = 0;
   /* TODO(jfb) Use a safe cast here, this test should only run for x86. */
   NaClSetCPUFeatureX86((NaClCPUFeaturesX86 *) cpu_features,